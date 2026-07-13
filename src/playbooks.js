stefanwalther/lavish-axi

fix: sync Mermaid diagrams with page themes (#162)
* fix(design): make recommended Mermaid init theme-aware

The `lavish-axi design` / diagram-playbook Mermaid snippet initialized with a
fixed `theme: "base"`, so every generated artifact rendered its diagrams in one
theme regardless of the page theme. On a dark page (e.g. DaisyUI `luxury`) the
cream "base" nodes and light edge-label boxes clashed badly; the fixed theme
also never followed a viewer theme toggle, since Mermaid does not restyle an
already-rendered SVG on its own.

The recommended snippet now:
- Picks the Mermaid theme ("dark"/"default") from the page's effective light or
  dark appearance, read from the actually-rendered page background so it works
  with any theming mechanism (prefers-color-scheme, a data-theme attribute, or
  plain CSS).
- Normalizes that background color through a 1x1 canvas, so detection survives
  modern color syntaxes - notably DaisyUI v5 serializes computed colors as
  space-separated `oklch(...)`, which a naive rgb comma-parser cannot read and
  would silently fall back to the OS preference.
- Drives rendering itself (`startOnLoad: false` + `mermaid.run`) and re-renders
  on page-theme toggles (MutationObserver on the root `data-theme`/class/style)
  and OS light/dark changes, doing the first render once stylesheets are applied
  to avoid a wrong-theme flash.

The diagram playbook now states the same theme-aware expectation, and tests lock
in the theme-aware init and playbook guidance so it cannot silently regress.

Before/after browser evidence (both themes + live toggle), captured through the
real lavish-axi flow, lives under task-evidence/mermaid-theme/ in the worktree.

* docs(evidence): add before/after Mermaid theming screenshots

Browser evidence for the theme-aware Mermaid fix, captured through the real
lavish-axi flow (served chrome + sandboxed artifact iframe):

- before-dark / before-light: fixed theme "base" clashes on a dark page.
- after-dark: dark page renders a 

src/playbooks.js
@@ -22,6 +22,7 @@ export const PLAYBOOKS = [
       "Use page-scoped class names and avoid generic names like .node that can collide with diagram libraries.",
       "Prefer top-down flow for multi-step diagrams unless the flow is genuinely linear and short.",
       "Quote labels that contain punctuation or code-like names, and use explicit line breaks where the renderer supports them.",
+      "Initialize Mermaid to match the page theme and re-render when the theme changes: pick the Mermaid theme from the effective page appearance (light or dark) at render time, and use the theme-aware `lavish-axi design` Mermaid snippet rather than hardcoding a single theme, since Mermaid does not restyle an already-rendered SVG when the viewer toggles the page theme.",
     ],
     pitfalls: [
       "Do not cram every file or function into one diagram when a layered explanation would be clearer.",

src/skill.js
@@ -80,7 +80,7 @@ ${bullets(home.visual_guidance)}
 
 Run \`npx -y lavish-axi playbook <id>\` for focused, detailed guidance on any of these.
 ${PLAYBOOK_ROUTER_HELP}
-For flows, architecture, state, or sequence diagrams, do not hand-build boxes-and-arrows from div/flexbox; open the diagram playbook and use Mermaid unless SVG is needed for richly annotated nodes.
+For flows, architecture, state, or sequence diagrams, do not hand-build boxes-and-arrows from div/flexbox; open the diagram playbook and use the theme-aware Mermaid snippet from \`npx -y lavish-axi design\` unless SVG is needed for richly annotated nodes.
 
 ${playbookList(home.playbooks)}
 

test/server.test.js
@@ -5,6 +5,9 @@ import { homedir, tmpdir } from "node:os";
 import path from "node:path";
 import test from "node:test";
 
+process.env.LAVISH_AXI_HOST = "127.0.0.1";
+process.env.LAVISH_AXI_LINK_HOST = "127.0.0.1";
+
 import {
   createChromeHtml,
   createSdkJs,

knowttl/atelier-axi

src/playbooks.js
@@ -22,6 +22,7 @@ export const PLAYBOOKS = [
       "Use page-scoped class names and avoid generic names like .node that can collide with diagram libraries.",
       "Prefer top-down flow for multi-step diagrams unless the flow is genuinely linear and short.",
       "Quote labels that contain punctuation or code-like names, and use explicit line breaks where the renderer supports them.",
+      "Initialize Mermaid to match the page theme and re-render when the theme changes: pick the Mermaid theme from the effective page appearance (light or dark) at render time, and use the theme-aware `atelier-axi design` Mermaid snippet rather than hardcoding a single theme, since Mermaid does not restyle an already-rendered SVG when the viewer toggles the page theme.",
     ],
     pitfalls: [
       "Do not cram every file or function into one diagram when a layered explanation would be clearer.",

src/skill.js
@@ -93,7 +93,7 @@ ${bullets(home.visual_guidance)}
 
 Run \`npx -y atelier-axi playbook <id>\` for focused, detailed guidance on any of these.
 ${PLAYBOOK_ROUTER_HELP}
-For flows, architecture, state, or sequence diagrams, do not hand-build boxes-and-arrows from div/flexbox; open the diagram playbook and use Mermaid unless SVG is needed for richly annotated nodes.
+For flows, architecture, state, or sequence diagrams, do not hand-build boxes-and-arrows from div/flexbox; open the diagram playbook and use the theme-aware Mermaid snippet from \`npx -y atelier-axi design\` unless SVG is needed for richly annotated nodes.
 
 ${playbookList(home.playbooks)}
 

test/server.test.js
@@ -5,6 +5,9 @@ import { homedir, tmpdir } from "node:os";
 import path from "node:path";
 import test from "node:test";
 
+process.env.ATELIER_AXI_HOST = "127.0.0.1";
+process.env.ATELIER_AXI_LINK_HOST = "127.0.0.1";
+
 import {
   createChromeHtml,
   createSdkJs,