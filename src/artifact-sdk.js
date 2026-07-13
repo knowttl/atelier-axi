stefanwalther/lavish-axi

feat: edit Mermaid diagrams as whiteboards (#166)
* feat: edit Mermaid diagrams as Excalidraw whiteboards with agent feedback

Mermaid diagrams in artifacts gain an 'Edit as whiteboard' action that opens
them in a full-viewport Excalidraw editor hosted in a dedicated sandboxed
frame (no allow-same-origin). Flowchart, sequence, class, ER, and state
diagrams convert to editable shapes with Mermaid node/edge identity preserved;
other diagram types open as an image to draw on.

- Vendored offline bundle (Excalidraw + @excalidraw/mermaid-to-excalidraw +
  exact-pinned mermaid 11.12.1 + React) built into dist/whiteboard with fonts;
  THIRD-PARTY-NOTICES.md covers licenses.
- Scenes autosave into per-diagram sidecar files under the state dir, restore
  across refresh/restart/resume, and go stale by source hash with an explicit
  re-convert vs keep-editing choice - never silently merged.
- 'Queue feedback' writes .excalidraw + PNG feedback files server-side and
  queues a tag:'whiteboard' prompt through the normal pipeline with a bounded
  id-based edit summary, local file paths, and normalized excalidraw-scene
  target; poll next_step teaches agents to update the Mermaid source.
- Whiteboard write routes are same-origin guarded with their own 20mb JSON
  limit; /whiteboard-assets serves with ACAO:* + no-cache for the opaque
  origin; error handler now surfaces body-parser statuses (413) instead of 500.

* feat: embed whiteboards inline in place of Mermaid diagrams

Per live-review direction: instead of an on-demand overlay behind an 'Edit as
whiteboard' hover button, the SDK now swaps each rendered Mermaid diagram for
an embedded sandboxed whiteboard frame at view time. Standalone and exported
artifacts still render plain Mermaid - the swap only happens under Lavish.

- Inline frames boot locked in view mode behind a click-catcher so embedded
  canvases never trap page scrolling; one click unlocks editing, and scenes
  auto-fit the frame instead of opening at 100% zoom.
- 

src/artifact-sdk.js
@@ -390,8 +390,98 @@ export function createArtifactSdk(
     return { x: parts[0], y: parts[1], w: parts[2], h: parts[3] };
   }
 
+  // Inline whiteboard embedding. Each rendered diagram inside a `.mermaid`
+  // container is replaced, at view time only, by a nested sandboxed iframe
+  // hosting the Excalidraw whiteboard frame - the artifact file keeps its
+  // Mermaid source and still renders plain diagrams when opened standalone or
+  // exported. The index of the container among `.mermaid` elements in document
+  // order is the diagram's identity; the server recovers the matching source
+  // from the artifact file. This SDK owns their lifecycle during fullscreen
+  // transitions.
+  const whiteboardEmbeds = new Map(); // container -> { iframe, index }
+
+  function mermaidContainerIndex(container) {
+    return [...document.querySelectorAll(".mermaid")].indexOf(container);
+  }
+
+  function whiteboardEmbedHeightPx(svgRect) {
+    const headerPx = 96;
+    const min = 360;
+    const max = Math.max(min, Math.round((window.innerHeight || 800) * 0.8));
+    return Math.max(min, Math.min(Math.round(svgRect.height) + headerPx, max));
+  }
+
+  function embedWhiteboard(svg) {
+    const container = svg.closest(".mermaid");
+    if (!container) return;
+    const existing = whiteboardEmbeds.get(container);
+    if (existing && existing.iframe.isConnected) {
+      existing.index = mermaidContainerIndex(container);
+      return;
+    }
+    const index = mermaidContainerIndex(container);
+    if (index < 0) return;
+    const rect = svg.getBoundingClientRect();
+    // Mermaid renders asynchronously; a zero-ish rect means this svg has not
+    // been laid out yet. Skip it and retry shortly - layout completion does
+    // not necessarily mutate the DOM again, so the observer alone is not a
+    // guaranteed wake-up.
+    if (rect.height < 40) {
+      window.setTimeout(scheduleMermaidEnhance, 150);
+      return;
+    }
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute("data-lavish-ui", "whiteboard-inline");
+    iframe.setAttribute("title", "Excalidraw whiteboard");
+    // Stricter than (and independent of) this artifact frame's own sandbox.
+    iframe.setAttribute("sandbox", "allow-scripts allow-popups");
+    iframe.src = whiteboardFrameSrc({ index, diagramId: svg.id || "" });
+    iframe.style.cssText =
+      `display:block;width:100%;height:${whiteboardEmbedHeightPx(rect)}px;border:1px solid rgba(128,128,128,.35);` +
+      "border-radius:12px;background:transparent";
+    // The design snippet re-renders Mermaid inside the container on theme
+    // changes, so the frame lives as a sibling: re-renders stay harmless
+    // inside the hidden container instead of destroying the editor.
+    container.style.display = "none";
+    container.insertAdjacentElement("afterend", iframe);
+    whiteboardEmbeds.set(container, { iframe, index, diagramId: svg.id || "" });
+  }
+
+  function whiteboardEmbedEntries() {
+    return [...whiteboardEmbeds.values()].filter((entry) => entry.iframe.isConnected);
+  }
+
+  function whiteboardEntryByIndex(index) {
+    return whiteboardEmbedEntries().find((entry) => entry.index === Number(index)) || null;
+  }
+
+  function whiteboardFrameSrc(entry) {
+    const params = new URLSearchParams({
+      diagramIndex: String(entry.index),
+      diagramId: String(entry.diagramId || ""),
+    });
+    return `/whiteboard-frame?${params}`;
+  }
+
+  window.addEventListener("message", (event) => {
+    if (event.source !== parent) return;
+    const msg = event.data || {};
+    // While the chrome overlay edits a diagram fullscreen, its inline frame is
+    // parked on about:blank so two editors never autosave the same sidecar;
+    // resume reboots the frame, which re-inits from the latest saved scene.
+    if (msg.type === "lavish:suspendWhiteboard") {
+      const target = whiteboardEntryByIndex(msg.diagramIndex);
+      if (target) target.iframe.src = "about:blank";
+    }
+    if (msg.type === "lavish:resumeWhiteboard") {
+      const target = whiteboardEntryByIndex(msg.diagramIndex);
+      if (target) target.iframe.src = whiteboardFrameSrc(target);
+    }
+  });
+
   function enhanceMermaid() {
     for (const svg of findMermaidSvgs()) {
+      embedWhiteboard(svg);
       if (mermaidViewports.has(svg)) continue;
       const viewport = createViewport(svg);
       if (viewport) {

test/session-store.test.js
@@ -119,6 +119,57 @@ test("queued mermaid node prompts preserve node identity and drop unknown fields
   }
 });
 
+test("queued whiteboard prompts normalize the excalidraw-scene target to its fixed shape", async () => {
+  const dir = await mkdtemp(path.join(tmpdir(), "lavish-store-"));
+  try {
+    const stateFile = path.join(dir, "state.json");
+    const artifact = path.join(dir, "artifact.html");
+    await writeFile(artifact, "<div class='mermaid'>graph TD; A-->B;</div>");
+
+    const store = new SessionStore(stateFile);
+    const session = await store.upsertSession(artifact, "http://localhost:4387/session/test");
+
+    await store.queuePrompts(session.key, {
+      prompts: [
+        {
+          uid: "",
+          prompt: "Whiteboard edits:\nMoved rectangle (Auth)",
+          selector: "",
+          tag: "whiteboard",
+          text: "Whiteboard edits",
+          target: {
+            type: "excalidraw-scene",
+            diagramIndex: "1",
+            diagramId: "mermaid-2",
+            sourceHash: "abc123def4567890",
+            scenePath: "/state/whiteboards/k/1.excalidraw",
+            previewPath: "/state/whiteboards/k/1.png",
+            imageFallback: false,
+            stats: { added: 1, removed: 0, moved: 2, relabeled: 0, drawn: 1 },
+            hostile: { nested: "should not survive" },
+          },
+        },
+      ],
+    });
+
+    const result = feedbackResult(await store.takeFeedback(session.key));
+    assert.equal(result.prompts.length, 1);
+    assert.equal(result.prompts[0].tag, "whiteboard");
+    assert.deepEqual(result.prompts[0].target, {
+      type: "excalidraw-scene",
+      diagramIndex: 1,
+      diagramId: "mermaid-2",
+      sourceHash: "abc123def4567890",
+      scenePath: "/state/whiteboards/k/1.excalidraw",
+      previewPath: "/state/whiteboards/k/1.png",
+      imageFallback: false,
+      stats: { added: 1, removed: 0, moved: 2, relabeled: 0, drawn: 1 },
+    });
+  } finally {
+    await rm(dir, { recursive: true, force: true });
+  }
+});
+
 test("layout warnings are returned as feedback and then cleared", async () => {
   const dir = await mkdtemp(path.join(tmpdir(), "lavish-store-"));
   try {

test/whiteboard-store.test.js
@@ -1,0 +1,135 @@
+import assert from "node:assert/strict";
+import { mkdtemp, readFile, rm } from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+import test from "node:test";
+
+import {
+  decodePngDataUrl,
+  isValidDiagramIndex,
+  isValidWhiteboardKey,
+  loadWhiteboard,
+  saveWhiteboard,
+  whiteboardFeedbackPaths,
+  writeWhiteboardFeedbackFiles,
+} from "../src/whiteboard-store.js";
+
+const KEY = "0123456789abcdef";
+// A 1x1 transparent PNG.
+const PNG_DATA_URL =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==";
+
+async function withTempDir(run) {
+  const dir = await mkdtemp(path.join(os.tmpdir(), "lavish-whiteboard-"));
+  try {
+    await run(dir);
+  } finally {
+    await rm(dir, { recursive: true, force: true });
+  }
+}
+
+test("saveWhiteboard/loadWhiteboard strips persisted theme and canvas background", async () => {
+  await withTempDir(async (dir) => {
+    const scene = {
+      elements: [{ id: "A", type: "rectangle" }],
+      appState: { theme: "dark", viewBackgroundColor: "#121212", scrollX: 12 },
+      files: {},
+    };
+    const baseline = { elements: [{ id: "A", type: "rectangle" }] };
+    await saveWhiteboard(dir, KEY, 0, { sourceHash: "hash-1", scene, baseline });
+    const loaded = await loadWhiteboard(dir, KEY, 0);
+    assert.equal(loaded.source_hash, "hash-1");
+    assert.deepEqual(loaded.scene, {
+      ...scene,
+      appState: { scrollX: 12 },
+    });
+    assert.deepEqual(loaded.baseline, baseline);
+    assert.ok(loaded.updated_at);
+  });
+});
+
+test("loadWhiteboard returns null when nothing was saved", async () => {
+  await withTempDir(async (dir) => {
+    assert.equal(await loadWhiteboard(dir, KEY, 3), null);
+  });
+});
+
+test("saveWhiteboard overwrites prior state for the same diagram", async () => {
+  await withTempDir(async (dir) => {
+    await saveWhiteboard(dir, KEY, 1, { sourceHash: "h1", scene: { elements: [] }, baseline: null });
+    await saveWhiteboard(dir, KEY, 1, { sourceHash: "h2", scene: { elements: [{ id: "B" }] }, baseline: null });
+    const loaded = await loadWhiteboard(dir, KEY, 1);
+    assert.equal(loaded.source_hash, "h2");
+    assert.equal(loaded.scene.elements.length, 1);
+  });
+});
+
+test("concurrent saves preserve the most recent scene", async () => {
+  await withTempDir(async (dir) => {
+    const slowScene = { elements: [{ id: "old", text: "x".repeat(8 * 1024 * 1024) }] };
+    const latestScene = { elements: [{ id: "latest" }] };
+    await Promise.all([
+      saveWhiteboard(dir, KEY, 5, { sourceHash: "old", scene: slowScene, baseline: null }),
+      saveWhiteboard(dir, KEY, 5, { sourceHash: "latest", scene: latestScene, baseline: null }),
+    ]);
+    const loaded = await loadWhiteboard(dir, KEY, 5);
+    assert.equal(loaded.source_hash, "latest");
+    assert.deepEqual(loaded.scene, latestScene);
+  });
+});
+
+test("store rejects invalid keys and indexes (path traversal guard)", async () => {
+  await withTempDir(async (dir) => {
+    await assert.rejects(() => saveWhiteboard(dir, "../../etc", 0, { sourceHash: "", scene: null }), /invalid/);
+    await assert.rejects(() => saveWhiteboard(dir, KEY, "../7", { sourceHash: "", scene: null }), /invalid/);
+    await assert.rejects(() => loadWhiteboard(dir, "ZZZZ", 0), /invalid/);
+    await assert.rejects(() => writeWhiteboardFeedbackFiles(dir, KEY, -1, { scene: null }), /invalid/);
+  });
+});
+
+test("isValidWhiteboardKey / isValidDiagramIndex validate shapes", () => {
+  assert.equal(isValidWhiteboardKey(KEY), true);
+  assert.equal(isValidWhiteboardKey("0123"), false);
+  assert.equal(isValidWhiteboardKey("0123456789ABCDEF"), false);
+  assert.equal(isValidDiagramIndex(0), true);
+  assert.equal(isValidDiagramIndex("12"), true);
+  assert.equal(isValidDiagramIndex(1000), false);
+  assert.equal(isValidDiagramIndex(-1), false);
+  assert.equal(isValidDiagramIndex(1.5), false);
+});
+
+test("writeWhiteboardFeedbackFiles writes a standalone .excalidraw and a PNG", async () => {
+  await withTempDir(async (dir) => {
+    const { scenePath, previewPath } = await writeWhiteboardFeedbackFiles(dir, KEY, 2, {
+      scene: { elements: [{ id: "A", type: "rectangle" }], appState: { theme: "light" }, files: {} },
+      pngDataUrl: PNG_DATA_URL,
+    });
+    assert.deepEqual({ scenePath, previewPath }, whiteboardFeedbackPaths(dir, KEY, 2));
+    const scene = JSON.parse(await readFile(scenePath, "utf8"));
+    assert.equal(scene.type, "excalidraw");
+    assert.equal(scene.version, 2);
+    assert.equal(scene.source, "lavish-axi");
+    assert.equal(scene.elements[0].id, "A");
+    assert.deepEqual(scene.appState, {});
+    const png = await readFile(previewPath);
+    assert.deepEqual([...png.subarray(0, 4)], [0x89, 0x50, 0x4e, 0x47]);
+  });
+});
+
+test("writeWhiteboardFeedbackFiles tolerates a missing or invalid preview", async () => {
+  await withTempDir(async (dir) => {
+    const { scenePath, previewPath } = await writeWhiteboardFeedbackFiles(dir, KEY, 4, {
+      scene: { elements: [] },
+      pngDataUrl: "data:image/svg+xml;base64,PHN2Zz48L3N2Zz4=",
+    });
+    assert.ok(scenePath.endsWith("4.excalidraw"));
+    assert.equal(previewPath, "");
+  });
+});
+
+test("decodePngDataUrl only accepts base64 PNG data URLs", () => {
+  assert.ok(decodePngDataUrl(PNG_DATA_URL) instanceof Buffer);
+  assert.equal(decodePngDataUrl("data:image/jpeg;base64,abcd"), null);
+  assert.equal(decodePngDataUrl("not-a-data-url"), null);
+  assert.equal(decodePngDataUrl(null), null);
+});

knowttl/atelier-axi

src/artifact-sdk.js
@@ -433,8 +433,98 @@ export function createArtifactSdk(
     return { x: parts[0], y: parts[1], w: parts[2], h: parts[3] };
   }
 
+  // Inline whiteboard embedding. Each rendered diagram inside a `.mermaid`
+  // container is replaced, at view time only, by a nested sandboxed iframe
+  // hosting the Excalidraw whiteboard frame - the artifact file keeps its
+  // Mermaid source and still renders plain diagrams when opened standalone or
+  // exported. The index of the container among `.mermaid` elements in document
+  // order is the diagram's identity; the server recovers the matching source
+  // from the artifact file. This SDK owns their lifecycle during fullscreen
+  // transitions.
+  const whiteboardEmbeds = new Map(); // container -> { iframe, index }
+
+  function mermaidContainerIndex(container) {
+    return [...document.querySelectorAll(".mermaid")].indexOf(container);
+  }
+
+  function whiteboardEmbedHeightPx(svgRect) {
+    const headerPx = 96;
+    const min = 360;
+    const max = Math.max(min, Math.round((window.innerHeight || 800) * 0.8));
+    return Math.max(min, Math.min(Math.round(svgRect.height) + headerPx, max));
+  }
+
+  function embedWhiteboard(svg) {
+    const container = svg.closest(".mermaid");
+    if (!container) return;
+    const existing = whiteboardEmbeds.get(container);
+    if (existing && existing.iframe.isConnected) {
+      existing.index = mermaidContainerIndex(container);
+      return;
+    }
+    const index = mermaidContainerIndex(container);
+    if (index < 0) return;
+    const rect = svg.getBoundingClientRect();
+    // Mermaid renders asynchronously; a zero-ish rect means this svg has not
+    // been laid out yet. Skip it and retry shortly - layout completion does
+    // not necessarily mutate the DOM again, so the observer alone is not a
+    // guaranteed wake-up.
+    if (rect.height < 40) {
+      window.setTimeout(scheduleMermaidEnhance, 150);
+      return;
+    }
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute("data-atelier-ui", "whiteboard-inline");
+    iframe.setAttribute("title", "Excalidraw whiteboard");
+    // Stricter than (and independent of) this artifact frame's own sandbox.
+    iframe.setAttribute("sandbox", "allow-scripts allow-popups");
+    iframe.src = whiteboardFrameSrc({ index, diagramId: svg.id || "" });
+    iframe.style.cssText =
+      `display:block;width:100%;height:${whiteboardEmbedHeightPx(rect)}px;border:1px solid rgba(128,128,128,.35);` +
+      "border-radius:12px;background:transparent";
+    // The design snippet re-renders Mermaid inside the container on theme
+    // changes, so the frame lives as a sibling: re-renders stay harmless
+    // inside the hidden container instead of destroying the editor.
+    container.style.display = "none";
+    container.insertAdjacentElement("afterend", iframe);
+    whiteboardEmbeds.set(container, { iframe, index, diagramId: svg.id || "" });
+  }
+
+  function whiteboardEmbedEntries() {
+    return [...whiteboardEmbeds.values()].filter((entry) => entry.iframe.isConnected);
+  }
+
+  function whiteboardEntryByIndex(index) {
+    return whiteboardEmbedEntries().find((entry) => entry.index === Number(index)) || null;
+  }
+
+  function whiteboardFrameSrc(entry) {
+    const params = new URLSearchParams({
+      diagramIndex: String(entry.index),
+      diagramId: String(entry.diagramId || ""),
+    });
+    return `/whiteboard-frame?${params}`;
+  }
+
+  window.addEventListener("message", (event) => {
+    if (event.source !== parent) return;
+    const msg = event.data || {};
+    // While the chrome overlay edits a diagram fullscreen, its inline frame is
+    // parked on about:blank so two editors never autosave the same sidecar;
+    // resume reboots the frame, which re-inits from the latest saved scene.
+    if (msg.type === "atelier:suspendWhiteboard") {
+      const target = whiteboardEntryByIndex(msg.diagramIndex);
+      if (target) target.iframe.src = "about:blank";
+    }
+    if (msg.type === "atelier:resumeWhiteboard") {
+      const target = whiteboardEntryByIndex(msg.diagramIndex);
+      if (target) target.iframe.src = whiteboardFrameSrc(target);
+    }
+  });
+
   function enhanceMermaid() {
     for (const svg of findMermaidSvgs()) {
+      embedWhiteboard(svg);
       if (mermaidViewports.has(svg)) continue;
       const viewport = createViewport(svg);
       if (viewport) {

test/session-store.test.js
@@ -119,6 +119,57 @@ test("queued mermaid node prompts preserve node identity and drop unknown fields
   }
 });
 
+test("queued whiteboard prompts normalize the excalidraw-scene target to its fixed shape", async () => {
+  const dir = await mkdtemp(path.join(tmpdir(), "atelier-store-"));
+  try {
+    const stateFile = path.join(dir, "state.json");
+    const artifact = path.join(dir, "artifact.html");
+    await writeFile(artifact, "<div class='mermaid'>graph TD; A-->B;</div>");
+
+    const store = new SessionStore(stateFile);
+    const session = await store.upsertSession(artifact, "http://localhost:4387/session/test");
+
+    await store.queuePrompts(session.key, {
+      prompts: [
+        {
+          uid: "",
+          prompt: "Whiteboard edits:\nMoved rectangle (Auth)",
+          selector: "",
+          tag: "whiteboard",
+          text: "Whiteboard edits",
+          target: {
+            type: "excalidraw-scene",
+            diagramIndex: "1",
+            diagramId: "mermaid-2",
+            sourceHash: "abc123def4567890",
+            scenePath: "/state/whiteboards/k/1.excalidraw",
+            previewPath: "/state/whiteboards/k/1.png",
+            imageFallback: false,
+            stats: { added: 1, removed: 0, moved: 2, relabeled: 0, drawn: 1 },
+            hostile: { nested: "should not survive" },
+          },
+        },
+      ],
+    });
+
+    const result = feedbackResult(await store.takeFeedback(session.key));
+    assert.equal(result.prompts.length, 1);
+    assert.equal(result.prompts[0].tag, "whiteboard");
+    assert.deepEqual(result.prompts[0].target, {
+      type: "excalidraw-scene",
+      diagramIndex: 1,
+      diagramId: "mermaid-2",
+      sourceHash: "abc123def4567890",
+      scenePath: "/state/whiteboards/k/1.excalidraw",
+      previewPath: "/state/whiteboards/k/1.png",
+      imageFallback: false,
+      stats: { added: 1, removed: 0, moved: 2, relabeled: 0, drawn: 1 },
+    });
+  } finally {
+    await rm(dir, { recursive: true, force: true });
+  }
+});
+
 test("layout warnings are returned as feedback and then cleared", async () => {
   const dir = await mkdtemp(path.join(tmpdir(), "atelier-store-"));
   try {

test/whiteboard-store.test.js
@@ -1,0 +1,135 @@
+import assert from "node:assert/strict";
+import { mkdtemp, readFile, rm } from "node:fs/promises";
+import os from "node:os";
+import path from "node:path";
+import test from "node:test";
+
+import {
+  decodePngDataUrl,
+  isValidDiagramIndex,
+  isValidWhiteboardKey,
+  loadWhiteboard,
+  saveWhiteboard,
+  whiteboardFeedbackPaths,
+  writeWhiteboardFeedbackFiles,
+} from "../src/whiteboard-store.js";
+
+const KEY = "0123456789abcdef";
+// A 1x1 transparent PNG.
+const PNG_DATA_URL =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg==";
+
+async function withTempDir(run) {
+  const dir = await mkdtemp(path.join(os.tmpdir(), "atelier-whiteboard-"));
+  try {
+    await run(dir);
+  } finally {
+    await rm(dir, { recursive: true, force: true });
+  }
+}
+
+test("saveWhiteboard/loadWhiteboard strips persisted theme and canvas background", async () => {
+  await withTempDir(async (dir) => {
+    const scene = {
+      elements: [{ id: "A", type: "rectangle" }],
+      appState: { theme: "dark", viewBackgroundColor: "#121212", scrollX: 12 },
+      files: {},
+    };
+    const baseline = { elements: [{ id: "A", type: "rectangle" }] };
+    await saveWhiteboard(dir, KEY, 0, { sourceHash: "hash-1", scene, baseline });
+    const loaded = await loadWhiteboard(dir, KEY, 0);
+    assert.equal(loaded.source_hash, "hash-1");
+    assert.deepEqual(loaded.scene, {
+      ...scene,
+      appState: { scrollX: 12 },
+    });
+    assert.deepEqual(loaded.baseline, baseline);
+    assert.ok(loaded.updated_at);
+  });
+});
+
+test("loadWhiteboard returns null when nothing was saved", async () => {
+  await withTempDir(async (dir) => {
+    assert.equal(await loadWhiteboard(dir, KEY, 3), null);
+  });
+});
+
+test("saveWhiteboard overwrites prior state for the same diagram", async () => {
+  await withTempDir(async (dir) => {
+    await saveWhiteboard(dir, KEY, 1, { sourceHash: "h1", scene: { elements: [] }, baseline: null });
+    await saveWhiteboard(dir, KEY, 1, { sourceHash: "h2", scene: { elements: [{ id: "B" }] }, baseline: null });
+    const loaded = await loadWhiteboard(dir, KEY, 1);
+    assert.equal(loaded.source_hash, "h2");
+    assert.equal(loaded.scene.elements.length, 1);
+  });
+});
+
+test("concurrent saves preserve the most recent scene", async () => {
+  await withTempDir(async (dir) => {
+    const slowScene = { elements: [{ id: "old", text: "x".repeat(8 * 1024 * 1024) }] };
+    const latestScene = { elements: [{ id: "latest" }] };
+    await Promise.all([
+      saveWhiteboard(dir, KEY, 5, { sourceHash: "old", scene: slowScene, baseline: null }),
+      saveWhiteboard(dir, KEY, 5, { sourceHash: "latest", scene: latestScene, baseline: null }),
+    ]);
+    const loaded = await loadWhiteboard(dir, KEY, 5);
+    assert.equal(loaded.source_hash, "latest");
+    assert.deepEqual(loaded.scene, latestScene);
+  });
+});
+
+test("store rejects invalid keys and indexes (path traversal guard)", async () => {
+  await withTempDir(async (dir) => {
+    await assert.rejects(() => saveWhiteboard(dir, "../../etc", 0, { sourceHash: "", scene: null }), /invalid/);
+    await assert.rejects(() => saveWhiteboard(dir, KEY, "../7", { sourceHash: "", scene: null }), /invalid/);
+    await assert.rejects(() => loadWhiteboard(dir, "ZZZZ", 0), /invalid/);
+    await assert.rejects(() => writeWhiteboardFeedbackFiles(dir, KEY, -1, { scene: null }), /invalid/);
+  });
+});
+
+test("isValidWhiteboardKey / isValidDiagramIndex validate shapes", () => {
+  assert.equal(isValidWhiteboardKey(KEY), true);
+  assert.equal(isValidWhiteboardKey("0123"), false);
+  assert.equal(isValidWhiteboardKey("0123456789ABCDEF"), false);
+  assert.equal(isValidDiagramIndex(0), true);
+  assert.equal(isValidDiagramIndex("12"), true);
+  assert.equal(isValidDiagramIndex(1000), false);
+  assert.equal(isValidDiagramIndex(-1), false);
+  assert.equal(isValidDiagramIndex(1.5), false);
+});
+
+test("writeWhiteboardFeedbackFiles writes a standalone .excalidraw and a PNG", async () => {
+  await withTempDir(async (dir) => {
+    const { scenePath, previewPath } = await writeWhiteboardFeedbackFiles(dir, KEY, 2, {
+      scene: { elements: [{ id: "A", type: "rectangle" }], appState: { theme: "light" }, files: {} },
+      pngDataUrl: PNG_DATA_URL,
+    });
+    assert.deepEqual({ scenePath, previewPath }, whiteboardFeedbackPaths(dir, KEY, 2));
+    const scene = JSON.parse(await readFile(scenePath, "utf8"));
+    assert.equal(scene.type, "excalidraw");
+    assert.equal(scene.version, 2);
+    assert.equal(scene.source, "atelier-axi");
+    assert.equal(scene.elements[0].id, "A");
+    assert.deepEqual(scene.appState, {});
+    const png = await readFile(previewPath);
+    assert.deepEqual([...png.subarray(0, 4)], [0x89, 0x50, 0x4e, 0x47]);
+  });
+});
+
+test("writeWhiteboardFeedbackFiles tolerates a missing or invalid preview", async () => {
+  await withTempDir(async (dir) => {
+    const { scenePath, previewPath } = await writeWhiteboardFeedbackFiles(dir, KEY, 4, {
+      scene: { elements: [] },
+      pngDataUrl: "data:image/svg+xml;base64,PHN2Zz48L3N2Zz4=",
+    });
+    assert.ok(scenePath.endsWith("4.excalidraw"));
+    assert.equal(previewPath, "");
+  });
+});
+
+test("decodePngDataUrl only accepts base64 PNG data URLs", () => {
+  assert.ok(decodePngDataUrl(PNG_DATA_URL) instanceof Buffer);
+  assert.equal(decodePngDataUrl("data:image/jpeg;base64,abcd"), null);
+  assert.equal(decodePngDataUrl("not-a-data-url"), null);
+  assert.equal(decodePngDataUrl(null), null);
+});