Julian-Dasilva/lavish-axi

fix: add semantic names to table annotations (#256)
* fix(sdk): give table-cell annotations semantic row and column names

Port the accepted work from #242 onto current main so filtered or sorted
tables keep semantic row and column names while the clicked element's own
selector, tag, and text stay the locator.

Co-authored-by: Adelin <adelin-b@users.noreply.github.com>

* no-mistakes(review): Remove source-token SDK serialization test

* no-mistakes(document): Clarify semantic table target documentation

* no-mistakes: apply CI fixes

---------

Co-authored-by: Cursor Agent <cursoragent@cursor.com>
Co-authored-by: Adelin <adelin-b@users.noreply.github.com>

test/artifact-sdk-bundle.test.js
@@ -1,0 +1,286 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import vm from "node:vm";
+
+import { createSdkJs } from "../src/server.js";
+
+// The SDK the browser actually runs is a serialized bundle, not the module: `createSdkJs` has to
+// declare every helper `createArtifactSdk` reaches for. A helper left out compiles fine and only
+// ReferenceErrors on the first click, so these tests boot the served bundle and drive the real
+// annotation path through a DOM stub instead of inspecting the module directly.
+
+function createElement(tag) {
+  const attributes = new Map();
+  const queried = new Map();
+  const element = {
+    tagName: String(tag).toUpperCase(),
+    nodeName: String(tag).toUpperCase(),
+    nodeType: 1,
+    parentElement: null,
+    children: [],
+    style: {},
+    value: "",
+    innerHTML: "",
+    textContent: "",
+    offsetWidth: 100,
+    offsetHeight: 100,
+    hidden: false,
+    listeners: [],
+    classList: {
+      add() {},
+      remove() {},
+      contains() {
+        return false;
+      },
+    },
+    setAttribute(name, value) {
+      attributes.set(name, String(value));
+    },
+    getAttribute(name) {
+      return attributes.has(name) ? attributes.get(name) : null;
+    },
+    matches(selectorList) {
+      return String(selectorList)
+        .split(",")
+        .some((part) => {
+          const selector = part.trim();
+          if (selector.startsWith("[")) return attributes.has(selector.slice(1, selector.indexOf("]")).split("=")[0]);
+          return selector === element.tagName.toLowerCase();
+        });
+    },
+    closest(selectorList) {
+      let current = element;
+      while (current) {
+        if (current.matches(selectorList)) return current;
+        current = current.parentElement;
+      }
+      return null;
+    },
+    appendChild(child) {
+      child.parentElement = element;
+      element.children.push(child);
+      return child;
+    },
+    remove() {
+      const index = element.parentElement?.children.indexOf(element) ?? -1;
+      if (index >= 0) element.parentElement.children.splice(index, 1);
+    },
+    // Card internals are looked up by class after innerHTML is assigned, so hand back a stable
+    // stub per selector: the test drives the very buttons the SDK wired up.
+    querySelector(selector) {
+      if (!queried.has(selector)) queried.set(selector, createElement(selector.replace(/^[.#]/, "")));
+      return queried.get(selector);
+    },
+    querySelectorAll() {
+      return [];
+    },
+    getBoundingClientRect() {
+      return { left: 10, top: 10, right: 110, bottom: 40, width: 100, height: 30 };
+    },
+    addEventListener(type, handler) {
+      element.listeners.push({ type, handler });
+    },
+    removeEventListener() {},
+    focus() {},
+    click() {},
+    scrollIntoView() {},
+    attachShadow() {
+      element.shadowRoot = createElement("shadow-root");
+      return element.shadowRoot;
+    },
+  };
+  return element;
+}
+
+function appendTo(parent, child) {
+  child.parentElement = parent;
+  parent.children.push(child);
+  return child;
+}
+
+function cell(tag, text) {
+  const element = createElement(tag);
+  element.textContent = text;
+  return element;
+}
+
+function bootSdk() {
+  const posted = [];
+  const documentListeners = [];
+  const documentElement = createElement("html");
+  const head = createElement("head");
+  const body = createElement("body");
+  appendTo(documentElement, head);
+  appendTo(documentElement, body);
+
+  const sandbox = {
+    parent: { postMessage: (message) => posted.push(message) },
+    navigator: { platform: "Linux" },
+    CSS: { escape: (value) => String(value) },
+    Element: class Element {},
+    MutationObserver: class MutationObserver {
+      observe() {}
+      disconnect() {}
+    },
+    ResizeObserver: class ResizeObserver {
+      observe() {}
+      disconnect() {}
+    },
+    URL: {
+      createObjectURL() {
+        return "blob:lavish-test";
+      },
+      revokeObjectURL() {},
+    },
+    getComputedStyle: () => ({}),
+    setTimeout: () => 0,
+    clearTimeout() {},
+    requestAnimationFrame: () => 0,
+    document: {
+      readyState: "complete",
+      documentElement,
+      head,
+      body,
+      activeElement: body,
+      baseURI: "http://127.0.0.1/artifact/abc/index.html",
+      addEventListener: (type, handler) => documentListeners.push({ type, handler }),
+      removeEventListener() {},
+      createElement,
+      getElementById: () => null,
+      querySelector: () => null,
+      querySelectorAll: () => [],
+      getSelection: () => null,
+    },
+  };
+  sandbox.window = {
+    addEventListener() {},
+    removeEventListener() {},
+    setTimeout: () => 0,
+    clearTimeout() {},
+    requestAnimationFrame: () => 0,
+    innerWidth: 1280,
+    innerHeight: 800,
+    scrollX: 0,
+    scrollY: 0,
+    location: { origin: "http://127.0.0.1" },
+    URL: sandbox.URL,
+  };
+  sandbox.globalThis = sandbox;
+
+  vm.runInNewContext(createSdkJs("abc", 3, "load-token"), sandbox);
+
+  return {
+    posted,
+    body,
+    api: sandbox.window.lavish,
+    click(target) {
+      const listener = documentListeners.find((entry) => entry.type === "click");
+      assert.ok(listener, "the SDK registers a document click listener");
+      listener.handler({ target, preventDefault() {}, stopPropagation() {} });
+    },
+    card() {
+      const card = documentElement.children
+        .flatMap((child) => child.shadowRoot?.children || [])
+        .findLast((child) => child.className === "lavish-annotation-card");
+      assert.ok(card, "clicking an element opens an annotation card");
+      return card;
+    },
+    queue(text) {
+      const card = this.card();
+      card.querySelector("textarea").value = text;
+      card.querySelector(".lavish-send").onclick();
+      return posted.at(-1);
+    },
+  };
+}
+
+function buildTable(sdk) {
+  const table = appendTo(sdk.body, createElement("table"));
+  const thead = appendTo(table, createElement("thead"));
+  const headerRow = appendTo(thead, createElement("tr"));
+  for (const label of ["Permission / setting", "Visible state", "Database evidence"]) {
+    appendTo(headerRow, cell("th", label));
+  }
+  const tbody = appendTo(table, createElement("tbody"));
+  const dataRow = appendTo(tbody, createElement("tr"));
+  appendTo(dataRow, cell("td", "Media & Apple Music"));
+  appendTo(dataRow, cell("td", "4 apps"));
+  const evidence = appendTo(dataRow, cell("td", "Drive, Neovide, Cursor"));
+  const badge = appendTo(evidence, cell("code", "Drive"));
+  return { evidence, badge };
+}
+
+test("the served SDK bundle queues a table-cell annotation without a missing-helper ReferenceError", () => {
+  const sdk = bootSdk();
+  const { evidence } = buildTable(sdk);
+
+  sdk.click(evidence);
+  const message = sdk.queue("Check this permission");
+
+  assert.equal(message.type, "lavish:queuePrompt");
+  assert.equal(message.prompt.prompt, "Check this permission");
+  assert.deepEqual(
+    { ...message.prompt.target },
+    {
+      type: "table-cell",
+      selector: "body > table > tbody > tr > td:nth-of-type(3)",
+      rowLabel: "Media & Apple Music",
+      columnLabel: "Database evidence",
+      text: "Drive, Neovide, Cursor",
+    },
+  );
+});
+
+test("the served SDK bundle keeps the clicked element's own identity inside a table cell", () => {
+  const sdk = bootSdk();
+  const { badge } = buildTable(sdk);
+
+  sdk.click(badge);
+  const message = sdk.queue("Rename this app");
+
+  assert.equal(message.prompt.tag, "code");
+  assert.equal(message.prompt.selector, "table > tbody > tr > td:nth-of-type(3) > code");
+  assert.equal(message.prompt.text, "Drive");
+  assert.equal(message.prompt.target.selector, "body > table > tbody > tr > td:nth-of-type(3)");
+  assert.equal(message.prompt.target.columnLabel, "Database evidence");
+});
+
+test("the annotation card names the cell it annotates when the cell itself is clicked", () => {
+  const sdk = bootSdk();
+  const { evidence } = buildTable(sdk);
+
+  sdk.click(evidence);
+
+  assert.match(sdk.card().innerHTML, /Annotate cell: Media &amp; Apple Music → Database evidence/);
+  assert.match(sdk.card().innerHTML, /about this table cell/);
+});
+
+test("the annotation card names the clicked element, not the cell, for a nested click", () => {
+  const sdk = bootSdk();
+  const { badge } = buildTable(sdk);
+
+  sdk.click(badge);
+
+  assert.match(sdk.card().innerHTML, /Annotate &lt;code&gt; in Media &amp; Apple Music → Database evidence/);
+  assert.doesNotMatch(sdk.card().innerHTML, /about this table cell/);
+});
+
+test("the served SDK bundle resolves table coordinates only for annotation clicks", () => {
+  const sdk = bootSdk();
+  const { evidence } = buildTable(sdk);
+
+  sdk.api.queuePrompt("Programmatic note", { element: evidence });
+
+  assert.equal(sdk.posted.at(-1).prompt.target, undefined);
+});
+
+test("the served SDK bundle annotates elements outside tables with no table target", () => {
+  const sdk = bootSdk();
+  const paragraph = appendTo(sdk.body, cell("p", "Just prose"));
+
+  sdk.click(paragraph);
+  const message = sdk.queue("Reword this");
+
+  assert.equal(message.prompt.tag, "p");
+  assert.equal(message.prompt.target, undefined);
+});

test/chrome-client-queue.test.js
@@ -542,6 +542,49 @@ test("chrome client replaces queued prompts with the same internal key", async (
   assert.doesNotMatch(chrome.element("annotationPills").innerHTML, /Use plan A/);
 });
 
+test("chrome client shows semantic table coordinates before positional selector", async () => {
+  const chrome = await createChromeHarness();
+
+  chrome.sendFrameMessage({
+    type: "lavish:queuePrompt",
+    prompt: {
+      prompt: "Check this permission",
+      selector: "table > tbody > tr:nth-of-type(7) > td:nth-of-type(3) > code",
+      tag: "code",
+      text: "Drive",
+      target: {
+        type: "table-cell",
+        selector: "table > tbody > tr:nth-of-type(7) > td:nth-of-type(3)",
+        rowLabel: "Media & Apple Music",
+        columnLabel: "Database evidence",
+        text: "Drive, Neovide, Cursor, Alacritty",
+      },
+    },
+  });
+
+  assert.match(chrome.element("annotationPills").innerHTML, /Media &amp; Apple Music → Database evidence/);
+  assert.match(chrome.element("annotationPills").innerHTML, /tr:nth-of-type\(7\)/);
+});
+
+test("chrome client falls back to the locator when a table cell has no row or column name", async () => {
+  const chrome = await createChromeHarness();
+
+  chrome.sendFrameMessage({
+    type: "lavish:queuePrompt",
+    prompt: {
+      prompt: "Check this permission",
+      selector: "table > tbody > tr:nth-of-type(7) > td:nth-of-type(3)",
+      tag: "td",
+      text: "Drive",
+      target: { type: "table-cell", rowLabel: "", columnLabel: "", text: "Drive" },
+    },
+  });
+
+  const html = chrome.element("annotationPills").innerHTML;
+  assert.match(html, /tr:nth-of-type\(7\)/);
+  assert.doesNotMatch(html, /Locator/);
+});
+
 test("chrome client scrolls new chat bubbles into view above queued prompts", async () => {
   const chrome = await createChromeHarness();
   const panelScroll = chrome.element("panelScroll");

knowttl/atelier-axi

(cherry picked from commit d27e2e8b489a3504cb1620c61dffc6d79afcae49)

test/artifact-sdk-bundle.test.js
@@ -1,0 +1,309 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import vm from "node:vm";
+
+import { createSdkJs } from "../src/server.js";
+
+// The SDK the browser actually runs is a serialized bundle, not the module: `createSdkJs` has to
+// declare every helper `createArtifactSdk` reaches for. A helper left out compiles fine and only
+// ReferenceErrors on the first click, so these tests boot the served bundle and drive the real
+// annotation path through a DOM stub instead of inspecting the module directly.
+
+function createElement(tag) {
+  const attributes = new Map();
+  const queried = new Map();
+  const element = {
+    tagName: String(tag).toUpperCase(),
+    nodeName: String(tag).toUpperCase(),
+    nodeType: 1,
+    parentElement: null,
+    children: [],
+    style: {},
+    value: "",
+    innerHTML: "",
+    textContent: "",
+    offsetWidth: 100,
+    offsetHeight: 100,
+    hidden: false,
+    listeners: [],
+    classList: {
+      add() {},
+      remove() {},
+      contains() {
+        return false;
+      },
+    },
+    setAttribute(name, value) {
+      attributes.set(name, String(value));
+    },
+    getAttribute(name) {
+      return attributes.has(name) ? attributes.get(name) : null;
+    },
+    matches(selectorList) {
+      return String(selectorList)
+        .split(",")
+        .some((part) => {
+          const selector = part.trim();
+          if (selector.startsWith("[")) return attributes.has(selector.slice(1, selector.indexOf("]")).split("=")[0]);
+          return selector === element.tagName.toLowerCase();
+        });
+    },
+    closest(selectorList) {
+      let current = element;
+      while (current) {
+        if (current.matches(selectorList)) return current;
+        current = current.parentElement;
+      }
+      return null;
+    },
+    appendChild(child) {
+      child.parentElement = element;
+      element.children.push(child);
+      return child;
+    },
+    remove() {
+      const index = element.parentElement?.children.indexOf(element) ?? -1;
+      if (index >= 0) element.parentElement.children.splice(index, 1);
+    },
+    // Card internals are looked up by class after innerHTML is assigned, so hand back a stable
+    // stub per selector: the test drives the very buttons the SDK wired up.
+    querySelector(selector) {
+      if (!queried.has(selector)) queried.set(selector, createElement(selector.replace(/^[.#]/, "")));
+      return queried.get(selector);
+    },
+    querySelectorAll() {
+      return [];
+    },
+    getBoundingClientRect() {
+      return { left: 10, top: 10, right: 110, bottom: 40, width: 100, height: 30 };
+    },
+    addEventListener(type, handler) {
+      element.listeners.push({ type, handler });
+    },
+    removeEventListener() {},
+    focus() {},
+    click() {},
+    scrollIntoView() {},
+    attachShadow() {
+      element.shadowRoot = createElement("shadow-root");
+      return element.shadowRoot;
+    },
+  };
+  return element;
+}
+
+function appendTo(parent, child) {
+  child.parentElement = parent;
+  parent.children.push(child);
+  return child;
+}
+
+function cell(tag, text) {
+  const element = createElement(tag);
+  element.textContent = text;
+  return element;
+}
+
+function bootSdk() {
+  const posted = [];
+  const documentListeners = [];
+  const windowListeners = [];
+  const documentElement = createElement("html");
+  const head = createElement("head");
+  const body = createElement("body");
+  appendTo(documentElement, head);
+  appendTo(documentElement, body);
+
+  const sandbox = {
+    parent: { postMessage: (message) => posted.push(message) },
+    navigator: { platform: "Linux" },
+    CSS: { escape: (value) => String(value) },
+    Element: class Element {
+      static [Symbol.hasInstance](value) {
+        return value?.nodeType === 1;
+      }
+    },
+    MutationObserver: class MutationObserver {
+      observe() {}
+      disconnect() {}
+    },
+    ResizeObserver: class ResizeObserver {
+      observe() {}
+      disconnect() {}
+    },
+    URL: {
+      createObjectURL() {
+        return "blob:atelier-test";
+      },
+      revokeObjectURL() {},
+    },
+    getComputedStyle: () => ({}),
+    setTimeout: () => 0,
+    clearTimeout() {},
+    requestAnimationFrame: () => 0,
+    document: {
+      readyState: "complete",
+      documentElement,
+      head,
+      body,
+      activeElement: body,
+      baseURI: "http://127.0.0.1/artifact/abc/index.html",
+      addEventListener: (type, handler) => documentListeners.push({ type, handler }),
+      removeEventListener() {},
+      createElement,
+      getElementById: () => null,
+      querySelector: () => null,
+      querySelectorAll: () => [],
+      getSelection: () => null,
+    },
+  };
+  sandbox.window = {
+    addEventListener: (type, handler) => windowListeners.push({ type, handler }),
+    removeEventListener() {},
+    setTimeout: () => 0,
+    clearTimeout() {},
+    requestAnimationFrame: () => 0,
+    innerWidth: 1280,
+    innerHeight: 800,
+    scrollX: 0,
+    scrollY: 0,
+    location: { origin: "http://127.0.0.1" },
+    URL: sandbox.URL,
+    crypto: {
+      getRandomValues(array) {
+        array.fill(1);
+        return array;
+      },
+    },
+    performance: { timeOrigin: 1 },
+  };
+  sandbox.globalThis = sandbox;
+
+  vm.runInNewContext(createSdkJs("abc", 3, "load-token"), sandbox);
+  const messageListeners = windowListeners.filter((entry) => entry.type === "message");
+  assert.ok(messageListeners.length > 0, "the SDK registers a window message listener");
+  sandbox.__messageListeners = messageListeners.map((entry) => entry.handler);
+  vm.runInNewContext(
+    '__messageListeners.forEach((handler) => handler({ source: parent, data: { type: "atelier:setAnnotationMode", enabled: true } }))',
+    sandbox,
+  );
+  assert.ok(
+    head.children.some((child) => child.id === "atelier-cursor-style"),
+    "the harness enables annotation mode through the production message path",
+  );
+
+  return {
+    posted,
+    body,
+    api: sandbox.window.atelier,
+    click(target) {
+      const listener = documentListeners.find((entry) => entry.type === "click");
+      assert.ok(listener, "the SDK registers a document click listener");
+      listener.handler({ target, button: 0, defaultPrevented: false, preventDefault() {}, stopPropagation() {} });
+    },
+    card() {
+      const card = documentElement.children
+        .flatMap((child) => child.shadowRoot?.children || [])
+        .findLast((child) => child.className === "atelier-annotation-card");
+      assert.ok(card, "clicking an element opens an annotation card");
+      return card;
+    },
+    queue(text) {
+      const card = this.card();
+      card.querySelector("textarea").value = text;
+      card.querySelector(".atelier-send").onclick();
+      return posted.at(-1);
+    },
+  };
+}
+
+function buildTable(sdk) {
+  const table = appendTo(sdk.body, createElement("table"));
+  const thead = appendTo(table, createElement("thead"));
+  const headerRow = appendTo(thead, createElement("tr"));
+  for (const label of ["Permission / setting", "Visible state", "Database evidence"]) {
+    appendTo(headerRow, cell("th", label));
+  }
+  const tbody = appendTo(table, createElement("tbody"));
+  const dataRow = appendTo(tbody, createElement("tr"));
+  appendTo(dataRow, cell("td", "Media & Apple Music"));
+  appendTo(dataRow, cell("td", "4 apps"));
+  const evidence = appendTo(dataRow, cell("td", "Drive, Neovide, Cursor"));
+  const badge = appendTo(evidence, cell("code", "Drive"));
+  return { evidence, badge };
+}
+
+test("the served SDK bundle queues a table-cell annotation without a missing-helper ReferenceError", () => {
+  const sdk = bootSdk();
+  const { evidence } = buildTable(sdk);
+
+  sdk.click(evidence);
+  const message = sdk.queue("Check this permission");
+
+  assert.equal(message.type, "atelier:queuePrompt");
+  assert.equal(message.prompt.prompt, "Check this permission");
+  assert.deepEqual(
+    { ...message.prompt.target },
+    {
+      type: "table-cell",
+      selector: "body > table > tbody > tr > td:nth-of-type(3)",
+      rowLabel: "Media & Apple Music",
+      columnLabel: "Database evidence",
+      text: "Drive, Neovide, Cursor",
+    },
+  );
+});
+
+test("the served SDK bundle keeps the clicked element's own identity inside a table cell", () => {
+  const sdk = bootSdk();
+  const { badge } = buildTable(sdk);
+
+  sdk.click(badge);
+  const message = sdk.queue("Rename this app");
+
+  assert.equal(message.prompt.tag, "code");
+  assert.equal(message.prompt.selector, "table > tbody > tr > td:nth-of-type(3) > code");
+  assert.equal(message.prompt.text, "Drive");
+  assert.equal(message.prompt.target.selector, "body > table > tbody > tr > td:nth-of-type(3)");
+  assert.equal(message.prompt.target.columnLabel, "Database evidence");
+});
+
+test("the annotation card names the cell it annotates when the cell itself is clicked", () => {
+  const sdk = bootSdk();
+  const { evidence } = buildTable(sdk);
+
+  sdk.click(evidence);
+
+  assert.match(sdk.card().innerHTML, /Annotate cell: Media &amp; Apple Music → Database evidence/);
+  assert.match(sdk.card().innerHTML, /about this table cell/);
+});
+
+test("the annotation card names the clicked element, not the cell, for a nested click", () => {
+  const sdk = bootSdk();
+  const { badge } = buildTable(sdk);
+
+  sdk.click(badge);
+
+  assert.match(sdk.card().innerHTML, /Annotate &lt;code&gt; in Media &amp; Apple Music → Database evidence/);
+  assert.doesNotMatch(sdk.card().innerHTML, /about this table cell/);
+});
+
+test("the served SDK bundle resolves table coordinates only for annotation clicks", () => {
+  const sdk = bootSdk();
+  const { evidence } = buildTable(sdk);
+
+  sdk.api.queuePrompt("Programmatic note", { element: evidence });
+
+  assert.equal(sdk.posted.at(-1).prompt.target, undefined);
+});
+
+test("the served SDK bundle annotates elements outside tables with no table target", () => {
+  const sdk = bootSdk();
+  const paragraph = appendTo(sdk.body, cell("p", "Just prose"));
+
+  sdk.click(paragraph);
+  const message = sdk.queue("Reword this");
+
+  assert.equal(message.prompt.tag, "p");
+  assert.equal(message.prompt.target, undefined);
+});

test/chrome-client-queue.test.js
@@ -695,6 +695,49 @@ test("chrome client replaces queued prompts with the same internal key", async (
   assert.doesNotMatch(chrome.element("annotationPills").innerHTML, /Use plan A/);
 });
 
+test("chrome client shows semantic table coordinates before positional selector", async () => {
+  const chrome = await createChromeHarness();
+
+  chrome.sendFrameMessage({
+    type: "atelier:queuePrompt",
+    prompt: {
+      prompt: "Check this permission",
+      selector: "table > tbody > tr:nth-of-type(7) > td:nth-of-type(3) > code",
+      tag: "code",
+      text: "Drive",
+      target: {
+        type: "table-cell",
+        selector: "table > tbody > tr:nth-of-type(7) > td:nth-of-type(3)",
+        rowLabel: "Media & Apple Music",
+        columnLabel: "Database evidence",
+        text: "Drive, Neovide, Cursor, Alacritty",
+      },
+    },
+  });
+
+  assert.match(chrome.element("annotationPills").innerHTML, /Media &amp; Apple Music → Database evidence/);
+  assert.match(chrome.element("annotationPills").innerHTML, /tr:nth-of-type\(7\)/);
+});
+
+test("chrome client falls back to the locator when a table cell has no row or column name", async () => {
+  const chrome = await createChromeHarness();
+
+  chrome.sendFrameMessage({
+    type: "atelier:queuePrompt",
+    prompt: {
+      prompt: "Check this permission",
+      selector: "table > tbody > tr:nth-of-type(7) > td:nth-of-type(3)",
+      tag: "td",
+      text: "Drive",
+      target: { type: "table-cell", rowLabel: "", columnLabel: "", text: "Drive" },
+    },
+  });
+
+  const html = chrome.element("annotationPills").innerHTML;
+  assert.match(html, /tr:nth-of-type\(7\)/);
+  assert.doesNotMatch(html, /Locator/);
+});
+
 test("chrome client scrolls new chat bubbles into view above queued prompts", async () => {
   const chrome = await createChromeHarness();
   const panelScroll = chrome.element("panelScroll");