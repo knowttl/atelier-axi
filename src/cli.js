Julian-Dasilva/lavish-axi

fix(cli): put poll feedback ahead of the DOM snapshot in output (#266)
* fix(poll): prioritize feedback in delivery output

Fixes #228; deliberately does not touch #168 ack-before-consume.

* no-mistakes(document): document poll output ordering invariant

* chore: ignore agent runtime state

* no-mistakes: apply CI fixes

---------

Co-authored-by: Julian Alecssandre Dasilva <jdas@Julians-MacBook-Pro.local>

src/cli.js
@@ -45,7 +45,7 @@ export const POLL_WAKE_PATH_RULES = Object.freeze([
   "Never use `nohup`, shell `&`, `disown`, redirected fire-and-forget processes, or a detached terminal without an explicit verified callback merely to keep polling alive.",
   "If the harness has no completion-aware background facility, use the foreground poll or first wire a verified wake callback into the surrounding supervisor.",
   "Do not tell the user the artifact is being monitored until that wake path is live.",
-  "If the poll gets killed or times out anyway, just re-run it - queued feedback is never lost.",
+  "If the poll gets killed or times out before feedback arrives, re-run it - feedback remains queued until delivery. Poll delivery consumes the response, so read it completely.",
 ]);
 export const POLL_SEND_AND_END_RULE =
   "`Send & End` ends the session. Its final feedback is still delivered once. After that response, polling stops, and the agent must not reopen the session uninvited.";
@@ -342,7 +342,7 @@ export function pollWaitBannerText(file) {
   return (
     `[lavish-axi] Long-polling for user feedback on ${file}. This stays silent until the user sends feedback or ends the session - leave it running. ` +
     `Detected layout issues do NOT return this poll: they wait in the user's Layout issues inbox until the user queues them as ordinary feedback. ` +
-    `If it gets killed or times out, re-run \`lavish-axi poll ${file}\` - queued feedback is never lost.`
+    `If it gets killed or times out before feedback arrives, re-run \`lavish-axi poll ${file}\` - feedback remains queued until delivery. Poll delivery consumes the response, so read it completely.`
   );
 }
 
@@ -354,7 +354,7 @@ export function pollWaitTickText(elapsedMs) {
 export function pollInterruptedText(file) {
   return (
     `[lavish-axi] Poll interrupted before user feedback arrived. The user may still be reviewing - ` +
-    `re-run \`lavish-axi poll ${file}\` to keep waiting; queued feedback is never lost.`
+    `re-run \`lavish-axi poll ${file}\` to keep waiting; feedback remains queued until delivery. Poll delivery consumes the response, so read it completely.`
   );
 }
 
@@ -380,10 +380,10 @@ export function startPollWaitReporter({
 /**
  * @returns {{
  *   session: { file: string, status: string, session_ended?: boolean, ended_by?: string },
- *   next_step?: string,
- *   dom_snapshot?: string,
  *   prompts?: any[],
  *   artifact_failures?: any[],
+ *   next_step?: string,
+ *   dom_snapshot?: string,
  * }}
  */
 export function createPollOutput({ file, response, agent = "generic" }) {
@@ -402,10 +402,10 @@ export function createPollOutput({ file, response, agent = "generic" }) {
         status: "feedback",
         ...(sessionEnded ? { session_ended: true, ...(endedBy ? { ended_by: endedBy } : {}) } : {}),
       },
-      dom_snapshot: response.dom_snapshot || "",
       prompts: response.prompts || [],
       ...(artifactFailures.length > 0 ? { artifact_failures: artifactFailures } : {}),
       next_step: createFeedbackNextStep(file, artifactFailures, sessionEnded, endedBy, response.prompts || [], agent),
+      dom_snapshot: response.dom_snapshot || "",
     };
   }
   if (response.status === "ended") {
@@ -416,7 +416,7 @@ export function createPollOutput({ file, response, agent = "generic" }) {
   }
   return {
     session: { file, status: response.status || "waiting" },
-    next_step: `No user feedback arrived before the optional timeout. Run \`lavish-axi poll ${file}\` without --timeout-ms to wait indefinitely - queued feedback is never lost, so re-running the poll is always safe.`,
+    next_step: `No user feedback arrived before the optional timeout. Run \`lavish-axi poll ${file}\` without --timeout-ms to wait indefinitely - feedback remains queued until delivery, so re-running the poll is safe while waiting. Poll delivery consumes the response, so read it completely.`,
   };
 }
 

test/cli-output.test.js
@@ -270,7 +270,7 @@ test("home output warns agents that poll needs an observable wake path", () => {
   assertObservablePollWakePath(pollHelp);
   assert.doesNotMatch(pollHelp, /Codex/);
   assert.match(pollHelp, /re-run/);
-  assert.match(pollHelp, /queued feedback is never lost/);
+  assert.match(pollHelp, /feedback remains queued until delivery/);
   assert.match(pollHelp, /`Send & End` ends the session/);
   assert.match(pollHelp, /final feedback is still delivered once/);
   assert.doesNotMatch(pollHelp, /above 10 minutes/);
@@ -301,7 +301,7 @@ test("home output keeps static skill poll guidance safe and agent-neutral", () =
   assertObservablePollWakePath(pollHelp);
   assert.doesNotMatch(pollHelp, /keep the poll attached to the active turn/i);
   assert.doesNotMatch(pollHelp, /Codex detected/);
-  assert.match(pollHelp, /queued feedback is never lost/);
+  assert.match(pollHelp, /feedback remains queued until delivery/);
 });
 
 test("invoking agent detection recognizes Codex runtime markers only", () => {
@@ -334,7 +334,7 @@ test("top-level help renders static home output without dynamic sessions", async
     assert.match(result.stdout, /lavish-axi design/);
     assert.match(result.stdout, /strict priority order/);
     assert.match(result.stdout, /never kill it/);
-    assert.match(result.stdout, /queued feedback is never lost/);
+    assert.match(result.stdout, /feedback remains queued until delivery/);
     assert.doesNotMatch(result.stdout, /above 10 minutes/);
     assert.doesNotMatch(result.stdout, /lavish-design/);
     assert.doesNotMatch(result.stdout, /sessions\[/);
@@ -773,7 +773,7 @@ test("open output keeps the user URL in session data and next_step focused on po
   assert.match(output.next_step, /never kill it/);
   assertObservablePollWakePath(output.next_step);
   assert.doesNotMatch(output.next_step, /Codex/);
-  assert.match(output.next_step, /queued feedback is never lost/);
+  assert.match(output.next_step, /feedback remains queued until delivery/);
   assert.match(output.next_step, /Do not pass --timeout-ms/);
   assert.match(output.next_step, /If the user ends the session, stop polling and do not reopen it/);
   assert.match(output.next_step, /--reopen/);
@@ -1116,7 +1116,7 @@ test("poll help requires an observable wake path", () => {
   assert.match(help, /never kill it/);
   assertObservablePollWakePath(help);
   assert.doesNotMatch(help, /Codex/);
-  assert.match(help, /queued feedback is never lost/);
+  assert.match(help, /feedback remains queued until delivery/);
   assert.match(help, /Do not pass --timeout-ms/);
   assert.match(help, /tests and debugging only/);
   assert.match(help, /`Send & End` ends the session/);
@@ -1161,9 +1161,77 @@ test("feedback next step keeps the next poll completion observable", () => {
   assert.match(output.next_step, /without --timeout-ms/);
   assertObservablePollWakePath(output.next_step);
   assert.doesNotMatch(output.next_step, /Codex/);
-  assert.match(output.next_step, /queued feedback is never lost/);
+  assert.match(output.next_step, /feedback remains queued until delivery/);
   assert.match(output.next_step, /Do not respond to the user just yet\. Now you must run/);
   assert.doesNotMatch(output.next_step, /above 10 minutes/);
+});
+
+test("poll feedback and the next step are emitted before the bulky DOM snapshot", async () => {
+  const stateDir = await mkdtemp(`${os.tmpdir()}/lavish-axi-poll-output-test-`);
+  const artifact = `${stateDir}/artifact.html`;
+  await writeFile(artifact, "<html><body>hello</body></html>", "utf8");
+  const response = {
+    status: "feedback",
+    prompts: [{ prompt: "Ship it", tag: "message" }],
+    artifact_failures: [{ kind: "artifact-unavailable", detail: "HTTP 404", severity: "fatal" }],
+    dom_snapshot: "large snapshot",
+  };
+  const server = createServer((req, res) => {
+    if (req.url === "/health") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ ok: true, app: "lavish-axi", version: VERSION }));
+      return;
+    }
+    if (req.url?.startsWith("/api/poll?")) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify(response));
+      return;
+    }
+    res.writeHead(404);
+    res.end();
+  });
+  await new Promise((resolve) => server.listen(0, "127.0.0.1", () => resolve()));
+  try {
+    const address = server.address();
+    assert.ok(address && typeof address !== "string");
+    const child = spawn(
+      process.execPath,
+      [fileURLToPath(new URL("../bin/lavish-axi.js", import.meta.url)), "poll", artifact, "--timeout-ms", "1000"],
+      {
+        cwd: fileURLToPath(new URL("..", import.meta.url)),
+        env: { ...process.env, LAVISH_AXI_STATE_DIR: stateDir, LAVISH_AXI_PORT: String(address.port) },
+      },
+    );
+    let stdout = "";
+    let stderr = "";
+    assert.ok(child.stdout);
+    assert.ok(child.stderr);
+    child.stdout.on("data", (chunk) => {
+      stdout += chunk.toString();
+    });
+    child.stderr.on("data", (chunk) => {
+      stderr += chunk.toString();
+    });
+    const result = await new Promise((resolve) => {
+      child.on("close", (status, signal) => resolve({ status, signal }));
+    });
+
+    assert.equal(result.status, 0, stderr);
+    const promptsIndex = stdout.indexOf("prompts[");
+    const failuresIndex = stdout.indexOf("artifact_failures[");
+    const nextStepIndex = stdout.indexOf("next_step:");
+    const snapshotIndex = stdout.indexOf("dom_snapshot:");
+    assert.ok(promptsIndex >= 0, "poll stdout contains prompts");
+    assert.ok(failuresIndex >= 0, "poll stdout contains artifact_failures");
+    assert.ok(nextStepIndex >= 0, "poll stdout contains next_step");
+    assert.ok(snapshotIndex >= 0, "poll stdout contains dom_snapshot");
+    assert.ok(promptsIndex < failuresIndex, "prompts precede artifact_failures in poll stdout");
+    assert.ok(failuresIndex < nextStepIndex, "artifact_failures precede next_step in poll stdout");
+    assert.ok(nextStepIndex < snapshotIndex, "next_step precedes dom_snapshot in poll stdout");
+  } finally {
+    await new Promise((resolve) => server.close(resolve));
+    await rm(stateDir, { force: true, recursive: true });
+  }
 });
 
 test("feedback next step is Codex-aware when requested", () => {
@@ -1442,7 +1510,7 @@ test("poll wait messages tell watching agents the silence is normal", () => {
   assert.match(banner, /Long-polling for user feedback/);
   assert.match(banner, /stays silent/);
   assert.match(banner, /leave it running/i);
-  assert.match(banner, /queued feedback is never lost/);
+  assert.match(banner, /feedback remains queued until delivery/);
 
   const tick = pollWaitTickText(3 * 60_000);
   assert.match(tick, /\[lavish-axi\]/);
@@ -1454,7 +1522,7 @@ test("poll wait messages tell watching agents the silence is normal", () => {
   assert.match(interrupted, /Poll interrupted/);
   assert.match(interrupted, /user may still be reviewing/);
   assert.match(interrupted, /lavish-axi poll \/tmp\/report\.html/);
-  assert.match(interrupted, /queued feedback is never lost/);
+  assert.match(interrupted, /feedback remains queued until delivery/);
 });
 
 test("poll wait reporter writes a banner immediately and heartbeats on an interval", async () => {
@@ -1559,7 +1627,7 @@ test("spawned poll with piped stderr banners once and leaves re-run guidance whe
     // to the child process's JavaScript signal handler.
     if (process.platform !== "win32") {
       assert.match(stderr, /Poll interrupted/);
-      assert.match(stderr, /queued feedback is never lost/);
+      assert.match(stderr, /feedback remains queued until delivery/);
     }
   } finally {
     await server.close();
@@ -1575,7 +1643,7 @@ test("waiting next step reassures agents that re-running poll loses nothing", ()
 
   assert.match(output.next_step, /lavish-axi poll \/tmp\/report\.html/);
   assert.match(output.next_step, /without --timeout-ms/);
-  assert.match(output.next_step, /queued feedback is never lost/);
+  assert.match(output.next_step, /feedback remains queued until delivery/);
 });
 
 test("html file arguments normalize to the hidden open command", () => {

knowttl/atelier-axi

(cherry picked from commit 9f429413694b28e1d968af0ac5061e4efe4a7234)

src/cli.js
@@ -53,7 +53,7 @@ export const POLL_WAKE_PATH_RULES = Object.freeze([
   "Never use `nohup`, shell `&`, `disown`, redirected fire-and-forget processes, or a detached terminal without an explicit verified callback merely to keep polling alive.",
   "If the harness has no completion-aware background facility, use the foreground poll or first wire a verified wake callback into the surrounding supervisor.",
   "Do not tell the user the artifact is being monitored until that wake path is live.",
-  "If the poll gets killed or times out anyway, just re-run it - queued feedback is never lost.",
+  "If the poll gets killed or times out before feedback arrives, re-run it - feedback remains queued until delivery. Poll delivery consumes the response, so read it completely.",
   "Run at most one poll per artifact: a session delivers each batch of feedback to one owning poll and retires every competing poll with a POLL_SUPERSEDED error rather than splitting feedback between them.",
   "Every delivered poll response ends with a `prompts_delivered=<N> batch_file=<path>` line: compare `<N>` against the prompts you actually read, and if you read fewer, your own output capture truncated the response - read the whole batch from `batch_file` instead of asking the user to repeat themselves. A `batch_file` value of `-` means the recovery copy could not be written, not that `-` is a filename.",
 ]);
@@ -359,7 +359,7 @@ export function pollWaitBannerText(file) {
   return (
     `[atelier-axi] Long-polling for user feedback on ${file}. This stays silent until the user sends feedback or ends the session - leave it running. ` +
     `Detected layout issues do NOT return this poll: they wait in the user's Layout issues inbox until the user queues them as ordinary feedback. ` +
-    `If it gets killed or times out, re-run \`atelier-axi poll ${file}\` - queued feedback is never lost.`
+    `If it gets killed or times out before feedback arrives, re-run \`atelier-axi poll ${file}\` - feedback remains queued until delivery. Poll delivery consumes the response, so read it completely.`
   );
 }
 
@@ -371,7 +371,7 @@ export function pollWaitTickText(elapsedMs) {
 export function pollInterruptedText(file) {
   return (
     `[atelier-axi] Poll interrupted before user feedback arrived. The user may still be reviewing - ` +
-    `re-run \`atelier-axi poll ${file}\` to keep waiting; queued feedback is never lost.`
+    `re-run \`atelier-axi poll ${file}\` to keep waiting; feedback remains queued until delivery. Poll delivery consumes the response, so read it completely.`
   );
 }
 
@@ -397,13 +397,13 @@ export function startPollWaitReporter({
 /**
  * @returns {{
  *   session: { file: string, status: string, session_ended?: boolean, ended_by?: string },
+ *   prompts?: any[],
+ *   artifact_failures?: any[],
  *   next_step?: string,
  *   dom_snapshot?: string,
  *   dom_snapshot_truncated?: boolean,
  *   dom_snapshot_bytes?: number,
  *   dom_snapshot_hint?: string,
- *   prompts?: any[],
- *   artifact_failures?: any[],
  * }}
  */
 export function createPollOutput({ file, response, full = false, agent = "generic" }) {
@@ -432,10 +432,10 @@ export function createPollOutput({ file, response, full = false, agent = "generi
         status: "feedback",
         ...(sessionEnded ? { session_ended: true, ...(endedBy ? { ended_by: endedBy } : {}) } : {}),
       },
-      ...truncateDomSnapshot(response.dom_snapshot, { file, full }),
       prompts: response.prompts || [],
       ...(artifactFailures.length > 0 ? { artifact_failures: artifactFailures } : {}),
       next_step: createFeedbackNextStep(file, artifactFailures, sessionEnded, endedBy, response.prompts || [], agent),
+      ...truncateDomSnapshot(response.dom_snapshot, { file, full }),
     };
   }
   if (response.status === "ended") {
@@ -446,7 +446,7 @@ export function createPollOutput({ file, response, full = false, agent = "generi
   }
   return {
     session: { file, status: response.status || "waiting" },
-    next_step: `No user feedback arrived before the optional timeout. Run \`atelier-axi poll ${file}\` without --timeout-ms to wait indefinitely - queued feedback is never lost, so re-running the poll is always safe.`,
+    next_step: `No user feedback arrived before the optional timeout. Run \`atelier-axi poll ${file}\` without --timeout-ms to wait indefinitely - feedback remains queued until delivery, so re-running the poll is safe while waiting. Poll delivery consumes the response, so read it completely.`,
   };
 }
 

test/cli-output.test.js
@@ -291,7 +291,7 @@ test("home output warns agents that poll needs an observable wake path", () => {
   assertObservablePollWakePath(pollHelp);
   assert.doesNotMatch(pollHelp, /Codex/);
   assert.match(pollHelp, /re-run/);
-  assert.match(pollHelp, /queued feedback is never lost/);
+  assert.match(pollHelp, /feedback remains queued until delivery/);
   assert.match(pollHelp, /`Send & End` ends the session/);
   assert.match(pollHelp, /final feedback is still delivered once/);
   assert.doesNotMatch(pollHelp, /above 10 minutes/);
@@ -322,7 +322,7 @@ test("home output keeps static skill poll guidance safe and agent-neutral", () =
   assertObservablePollWakePath(pollHelp);
   assert.doesNotMatch(pollHelp, /keep the poll attached to the active turn/i);
   assert.doesNotMatch(pollHelp, /Codex detected/);
-  assert.match(pollHelp, /queued feedback is never lost/);
+  assert.match(pollHelp, /feedback remains queued until delivery/);
 });
 
 test("invoking agent detection recognizes Codex runtime markers only", () => {
@@ -364,7 +364,7 @@ test("top-level help renders static home output without dynamic sessions", async
     assert.match(result.stdout, /app's own design system/i);
     assert.doesNotMatch(result.stdout, /inspect the current project/i);
     assert.match(result.stdout, /never kill it/);
-    assert.match(result.stdout, /queued feedback is never lost/);
+    assert.match(result.stdout, /feedback remains queued until delivery/);
     assert.doesNotMatch(result.stdout, /above 10 minutes/);
     assert.doesNotMatch(result.stdout, /atelier-design/);
     assert.doesNotMatch(result.stdout, /sessions\[/);
@@ -876,7 +876,7 @@ test("open output keeps the user URL in session data and next_step focused on po
   assert.match(output.next_step, /never kill it/);
   assertObservablePollWakePath(output.next_step);
   assert.doesNotMatch(output.next_step, /Codex/);
-  assert.match(output.next_step, /queued feedback is never lost/);
+  assert.match(output.next_step, /feedback remains queued until delivery/);
   assert.match(output.next_step, /Do not pass --timeout-ms/);
   assert.doesNotMatch(output.next_step, /above 10 minutes/);
   assert.match(output.next_step, /If the user ends the session, stop polling and do not reopen it/);
@@ -1218,7 +1218,7 @@ test("poll help requires an observable wake path", () => {
   assert.match(help, /never kill it/);
   assertObservablePollWakePath(help);
   assert.doesNotMatch(help, /Codex/);
-  assert.match(help, /queued feedback is never lost/);
+  assert.match(help, /feedback remains queued until delivery/);
   assert.match(help, /Do not pass --timeout-ms/);
   assert.match(help, /tests and debugging only/);
   assert.match(help, /`Send & End` ends the session/);
@@ -1383,9 +1383,77 @@ test("feedback next step keeps the next poll completion observable", () => {
   assert.match(output.next_step, /without --timeout-ms/);
   assertObservablePollWakePath(output.next_step);
   assert.doesNotMatch(output.next_step, /Codex/);
-  assert.match(output.next_step, /queued feedback is never lost/);
+  assert.match(output.next_step, /feedback remains queued until delivery/);
   assert.match(output.next_step, /Do not respond to the user just yet\. Now you must run/);
   assert.doesNotMatch(output.next_step, /above 10 minutes/);
+});
+
+test("poll feedback and the next step are emitted before the bulky DOM snapshot", async () => {
+  const stateDir = await mkdtemp(`${os.tmpdir()}/atelier-axi-poll-output-test-`);
+  const artifact = `${stateDir}/artifact.html`;
+  await writeFile(artifact, "<html><body>hello</body></html>", "utf8");
+  const response = {
+    status: "feedback",
+    prompts: [{ prompt: "Ship it", tag: "message" }],
+    artifact_failures: [{ kind: "artifact-unavailable", detail: "HTTP 404", severity: "fatal" }],
+    dom_snapshot: "large snapshot",
+  };
+  const server = createServer((req, res) => {
+    if (req.url === "/health") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ ok: true, app: "atelier-axi", version: VERSION }));
+      return;
+    }
+    if (req.url?.startsWith("/api/poll?")) {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify(response));
+      return;
+    }
+    res.writeHead(404);
+    res.end();
+  });
+  await new Promise((resolve) => server.listen(0, "127.0.0.1", () => resolve()));
+  try {
+    const address = server.address();
+    assert.ok(address && typeof address !== "string");
+    const child = spawn(
+      process.execPath,
+      [fileURLToPath(new URL("../bin/atelier-axi.js", import.meta.url)), "poll", artifact, "--timeout-ms", "1000"],
+      {
+        cwd: fileURLToPath(new URL("..", import.meta.url)),
+        env: { ...process.env, ATELIER_AXI_STATE_DIR: stateDir, ATELIER_AXI_PORT: String(address.port) },
+      },
+    );
+    let stdout = "";
+    let stderr = "";
+    assert.ok(child.stdout);
+    assert.ok(child.stderr);
+    child.stdout.on("data", (chunk) => {
+      stdout += chunk.toString();
+    });
+    child.stderr.on("data", (chunk) => {
+      stderr += chunk.toString();
+    });
+    const result = await new Promise((resolve) => {
+      child.on("close", (status, signal) => resolve({ status, signal }));
+    });
+
+    assert.equal(result.status, 0, stderr);
+    const promptsIndex = stdout.indexOf("prompts[");
+    const failuresIndex = stdout.indexOf("artifact_failures[");
+    const nextStepIndex = stdout.indexOf("next_step:");
+    const snapshotIndex = stdout.indexOf("dom_snapshot:");
+    assert.ok(promptsIndex >= 0, "poll stdout contains prompts");
+    assert.ok(failuresIndex >= 0, "poll stdout contains artifact_failures");
+    assert.ok(nextStepIndex >= 0, "poll stdout contains next_step");
+    assert.ok(snapshotIndex >= 0, "poll stdout contains dom_snapshot");
+    assert.ok(promptsIndex < failuresIndex, "prompts precede artifact_failures in poll stdout");
+    assert.ok(failuresIndex < nextStepIndex, "artifact_failures precede next_step in poll stdout");
+    assert.ok(nextStepIndex < snapshotIndex, "next_step precedes dom_snapshot in poll stdout");
+  } finally {
+    await new Promise((resolve) => server.close(resolve));
+    await rm(stateDir, { force: true, recursive: true });
+  }
 });
 
 test("feedback next step is Codex-aware when requested", () => {
@@ -1709,7 +1777,7 @@ test("poll wait messages tell watching agents the silence is normal", () => {
   assert.match(banner, /Long-polling for user feedback/);
   assert.match(banner, /stays silent/);
   assert.match(banner, /leave it running/i);
-  assert.match(banner, /queued feedback is never lost/);
+  assert.match(banner, /feedback remains queued until delivery/);
 
   const tick = pollWaitTickText(3 * 60_000);
   assert.match(tick, /\[atelier-axi\]/);
@@ -1721,7 +1789,7 @@ test("poll wait messages tell watching agents the silence is normal", () => {
   assert.match(interrupted, /Poll interrupted/);
   assert.match(interrupted, /user may still be reviewing/);
   assert.match(interrupted, /atelier-axi poll \/tmp\/report\.html/);
-  assert.match(interrupted, /queued feedback is never lost/);
+  assert.match(interrupted, /feedback remains queued until delivery/);
 });
 
 test("poll wait reporter writes a banner immediately and heartbeats on an interval", async () => {
@@ -2039,7 +2107,7 @@ test("spawned poll with piped stderr banners once and leaves re-run guidance whe
     // to the child process's JavaScript signal handler.
     if (process.platform !== "win32") {
       assert.match(stderr, /Poll interrupted/);
-      assert.match(stderr, /queued feedback is never lost/);
+      assert.match(stderr, /feedback remains queued until delivery/);
     }
   } finally {
     await server.close();
@@ -2055,7 +2123,7 @@ test("waiting next step reassures agents that re-running poll loses nothing", ()
 
   assert.match(output.next_step, /atelier-axi poll \/tmp\/report\.html/);
   assert.match(output.next_step, /without --timeout-ms/);
-  assert.match(output.next_step, /queued feedback is never lost/);
+  assert.match(output.next_step, /feedback remains queued until delivery/);
 });
 
 test("html file arguments normalize to the hidden open command", () => {