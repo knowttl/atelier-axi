Julian-Dasilva/lavish-axi

fix(chrome): recover a review that never finishes loading (#268)
* fix(chrome): recover a review that never finishes loading

A review page could come up with a spinner that never resolved, or with the
chrome shell around a permanently empty artifact frame. There are two triggers.

The first is the shared background server being replaced while a review page is
opening, which any `lavish-axi <html-file>` invocation can do after an upgrade:
the server broadcasts `chrome-reload` to every connected chrome and steps down,
so an unrelated session's page reloads on its own and then races the
replacement. The second is a second browser tab claiming the same review, which
supersedes the reviewer handoff the first tab is holding.

Four failures on those paths left no way back:

- The chrome page ships with the layout-gate overlay already covering the
  artifact, and only `chrome-client.js` ever takes it down - including wiring
  the "Show anyway" button. When that script never runs, the page sits on the
  spinner with a button that does nothing. An inline failsafe now arms before
  the script tag, fires on its `onerror` and on a timeout, and replaces the
  spinner with a named failure and a working Reload button. The client cancels
  it once it has run to completion.

- `replaceArtifactFrame` gave up permanently after two retries spanning 400ms,
  which does not cover the multi-second window where the server is being
  replaced. On a first load there is no previous frame to fall back to, so the
  iframe was never navigated and `artifact_revision` never advanced. Recoverable
  outcomes now retry on a backoff, and only surface a failure once that is
  exhausted with nothing on screen. `superseded` and `out-of-order` still do not
  retry: another reviewer, or a newer request in this same chrome, owns the
  artifact.

- After `chrome-reload`, the chrome reloaded on a fixed 5s deadline whether or
  not anything was listening, replacing a recoverable page with the browser's
  conne

src/artifact-sdk.js
@@ -2063,6 +2063,18 @@ export function createArtifactSdk(
 
   let activeCardContext = null;
   let reviewStateTimer = 0;
+  let draftRestoreTimer = 0;
+  const REVIEW_DRAFT_ANCHOR_SETTLE_MS = 1500;
+
+  // A card the user opened ends the pending late restore for good, not just for the instant the
+  // settle timer happens to fire: closing that card reports `card: null`, which retires the stored
+  // draft, so a restore landing afterwards would put text the chrome no longer holds back on
+  // screen and back into persistence over a card the user already dismissed.
+  function cancelPendingDraftRestore() {
+    if (!draftRestoreTimer) return;
+    window.clearTimeout(draftRestoreTimer);
+    draftRestoreTimer = 0;
+  }
 
   function safeQuerySelector(selector) {
     try {
@@ -2150,7 +2162,29 @@ export function createArtifactSdk(
     const card = state.card;
     if (!card || !card.selector || !String(card.text || "").trim()) return;
     const target = safeQuerySelector(card.selector);
-    if (!target) return;
+    if (!target) {
+      // The load event says the document parsed, not that it finished rendering: a section this
+      // page builds in script, or a Mermaid diagram, arrives later. Ask again once it has had
+      // time to appear, and restore the card if it did. Only then is the anchor's absence an
+      // answer worth reporting - the chrome cannot see into this document, and a draft it is
+      // never told about is retried against every later load.
+      cancelPendingDraftRestore();
+      draftRestoreTimer = window.setTimeout(() => {
+        draftRestoreTimer = 0;
+        // The user may have opened a card of their own inside the settle window, and
+        // `showAnnotationCard` closes whatever is open before it draws. Restoring over live
+        // typing destroys text nothing has carried to the chrome yet, so a card on screen ends
+        // this attempt: the draft is still stored, and the next load tries again.
+        if (activeCardContext) return;
+        const late = safeQuerySelector(card.selector);
+        if (late) {
+          showAnnotationCard(late, { restoreText: String(card.text) });
+          return;
+        }
+        postArtifactMessage("lavish:reviewDraftUnrestorable", { selector: String(card.selector) });
+      }, REVIEW_DRAFT_ANCHOR_SETTLE_MS);
+      return;
+    }
     showAnnotationCard(target, { restoreText: String(card.text) });
   }
 
@@ -2196,6 +2230,7 @@ export function createArtifactSdk(
   }
 
   function showAnnotationCard(target, options = {}) {
+    cancelPendingDraftRestore();
     const root = ensureShadow();
     closeCard();
 

test/cli-output.test.js
@@ -37,6 +37,7 @@ import {
   resolveCopilotHookDir,
   resolveHookHomeDir,
   resolveServerEntry,
+  serverReplacementReason,
   shutdownServerOnPort,
   shouldForceRestartForLocalBuild,
   shouldKillProcessOnPort,
@@ -2167,6 +2168,25 @@ test("shouldRestartServer does not restart when /health was unreachable", () => 
   assert.equal(shouldRestartServer("0.1.4", null), false);
 });
 
+// Every other open review page is told why its server went away, so the reason has to name the
+// branch that actually fired: a local-build force replaces a server of the same version, and
+// calling that an update is false on both counts.
+test("serverReplacementReason names a local-build force apart from a real version change", () => {
+  assert.equal(serverReplacementReason("0.1.4", { ok: true, app: "lavish-axi", version: "0.1.3" }), "upgrade");
+  assert.equal(serverReplacementReason("0.1.4", { ok: true, app: "lavish-axi" }), "upgrade");
+  assert.equal(
+    serverReplacementReason("0.1.4", { ok: true, app: "lavish-axi", version: "0.1.4" }, true),
+    "local-build",
+  );
+  // A version difference is an upgrade even when the local-build force is also set.
+  assert.equal(serverReplacementReason("0.1.4", { ok: true, app: "lavish-axi", version: "0.1.3" }, true), "upgrade");
+});
+
+test("serverReplacementReason names nothing when no replacement is warranted", () => {
+  assert.equal(serverReplacementReason("0.1.4", { ok: true, app: "lavish-axi", version: "0.1.4" }), "");
+  assert.equal(serverReplacementReason("0.1.4", null), "");
+});
+
 test("shouldKillProcessOnPort does not kill unidentified health responders", () => {
   assert.equal(shouldKillProcessOnPort("0.1.4", { ok: true, app: "other", version: "0.1.3" }), false);
 });
@@ -2388,3 +2408,109 @@ async function startFakeHtmlApp(requests) {
     close: () => new Promise((resolve) => server.close(() => resolve())),
   };
 }
+
+// A stand-in for a running server of another version: it answers /health, records what the CLI
+// actually puts on the wire at /shutdown, and then frees the port like a real one.
+async function startShutdownRecorder(version = "0.0.0-previous") {
+  const bodies = [];
+  const server = createServer((req, res) => {
+    if (req.url === "/health") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ ok: true, app: "lavish-axi", version }));
+      return;
+    }
+    if (req.url === "/shutdown" && req.method === "POST") {
+      let raw = "";
+      req.on("data", (chunk) => {
+        raw += chunk;
+      });
+      req.on("end", () => {
+        bodies.push(raw ? JSON.parse(raw) : {});
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ status: "shutting-down" }));
+        server.close();
+        server.closeAllConnections?.();
+      });
+      return;
+    }
+    res.writeHead(404).end();
+  });
+  await new Promise((resolve) => server.listen(0, "127.0.0.1", () => resolve(undefined)));
+  const address = /** @type {import("node:net").AddressInfo} */ (server.address());
+  return { bodies, port: address.port, close: () => server.close() };
+}
+
+test("lavish-axi stop tells the server it was stopped, and names no session to reload", async () => {
+  const recorder = await startShutdownRecorder();
+  try {
+    const output = await shutdownServerOnPort(recorder.port, {
+      baseUrl: `http://127.0.0.1:${recorder.port}`,
+      currentVersion: "0.1.4",
+    });
+
+    assert.deepEqual(recorder.bodies, [{ reason: "stop" }]);
+    assert.equal(output.server.status, "stopped");
+  } finally {
+    recorder.close();
+  }
+});
+
+// The page being opened is the one that reloads itself, and the server picks it by key, so the
+// key the CLI sends has to be the canonical session key for the file - an empty or non-canonical
+// one silently degrades the feature to "nobody reloads, everybody gets a banner".
+test("opening an artifact names that session as the one to reload across a version upgrade", async () => {
+  const recorder = await startShutdownRecorder();
+  const dir = await mkdtemp(path.join(os.tmpdir(), "lavish-open-reload-"));
+  const stateDir = path.join(dir, "state");
+  const nested = path.join(dir, "pages");
+  await mkdir(nested, { recursive: true });
+  const artifact = path.join(nested, "board.html");
+  await writeFile(artifact, "<!doctype html><html><body>board</body></html>");
+  const base = `http://127.0.0.1:${recorder.port}`;
+
+  try {
+    // Spawned asynchronously on purpose: the recorder runs in this process, so a blocking
+    // spawnSync would deadlock the CLI's own /health request against it.
+    const child = spawn(
+      process.execPath,
+      // A path with a `..` hop: the key must come from the canonicalized file, not this spelling.
+      [
+        fileURLToPath(new URL("../bin/lavish-axi.js", import.meta.url)),
+        path.join(nested, "..", "pages", "board.html"),
+        "--no-open",
+      ],
+      {
+        cwd: fileURLToPath(new URL("..", import.meta.url)),
+        env: {
+          ...process.env,
+          LAVISH_AXI_PORT: String(recorder.port),
+          LAVISH_AXI_STATE_DIR: stateDir,
+          LAVISH_AXI_TELEMETRY: "0",
+        },
+        stdio: ["ignore", "pipe", "pipe"],
+      },
+    );
+    let stderr = "";
+    child.stderr.on("data", (chunk) => {
+      stderr += chunk;
+    });
+    child.stdout.resume();
+    const code = await new Promise((resolve) => child.on("exit", resolve));
+
+    assert.equal(code, 0, stderr);
+    assert.deepEqual(recorder.bodies, [{ reload_key: sessionKey(await canonicalFile(artifact)), reason: "upgrade" }]);
+  } finally {
+    // The CLI replaced the recorder with a real server on that port; stop it again.
+    await fetch(`${base}/shutdown`, { method: "POST" }).catch(() => {});
+    for (let i = 0; i < 30; i += 1) {
+      const alive = await fetch(`${base}/health`).then(
+        () => true,
+        () => false,
+      );
+      if (!alive) break;
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+    recorder.close();
+    await rm(dir, { recursive: true, force: true });
+  }
+});

knowttl/atelier-axi

(cherry picked from commit 30ef25389f409464a6f0a7d0121830e4a9335242)

src/artifact-sdk.js
@@ -2137,6 +2137,18 @@ export function createArtifactSdk(
 
   let activeCardContext = null;
   let reviewStateTimer = 0;
+  let draftRestoreTimer = 0;
+  const REVIEW_DRAFT_ANCHOR_SETTLE_MS = 1500;
+
+  // A card the user opened ends the pending late restore for good, not just for the instant the
+  // settle timer happens to fire: closing that card reports `card: null`, which retires the stored
+  // draft, so a restore landing afterwards would put text the chrome no longer holds back on
+  // screen and back into persistence over a card the user already dismissed.
+  function cancelPendingDraftRestore() {
+    if (!draftRestoreTimer) return;
+    window.clearTimeout(draftRestoreTimer);
+    draftRestoreTimer = 0;
+  }
 
   function safeQuerySelector(selector) {
     try {
@@ -2224,7 +2236,29 @@ export function createArtifactSdk(
     const card = state.card;
     if (!card || !card.selector || !String(card.text || "").trim()) return;
     const target = safeQuerySelector(card.selector);
-    if (!target) return;
+    if (!target) {
+      // The load event says the document parsed, not that it finished rendering: a section this
+      // page builds in script, or a Mermaid diagram, arrives later. Ask again once it has had
+      // time to appear, and restore the card if it did. Only then is the anchor's absence an
+      // answer worth reporting - the chrome cannot see into this document, and a draft it is
+      // never told about is retried against every later load.
+      cancelPendingDraftRestore();
+      draftRestoreTimer = window.setTimeout(() => {
+        draftRestoreTimer = 0;
+        // The user may have opened a card of their own inside the settle window, and
+        // `showAnnotationCard` closes whatever is open before it draws. Restoring over live
+        // typing destroys text nothing has carried to the chrome yet, so a card on screen ends
+        // this attempt: the draft is still stored, and the next load tries again.
+        if (activeCardContext) return;
+        const late = safeQuerySelector(card.selector);
+        if (late) {
+          showAnnotationCard(late, { restoreText: String(card.text) });
+          return;
+        }
+        postArtifactMessage("atelier:reviewDraftUnrestorable", { selector: String(card.selector) });
+      }, REVIEW_DRAFT_ANCHOR_SETTLE_MS);
+      return;
+    }
     showAnnotationCard(target, { restoreText: String(card.text) });
   }
 
@@ -2270,6 +2304,7 @@ export function createArtifactSdk(
   }
 
   function showAnnotationCard(target, options = {}) {
+    cancelPendingDraftRestore();
     const root = ensureShadow();
     closeCard();
 

test/cli-output.test.js
@@ -43,6 +43,7 @@ import {
   resolveCopilotHookDir,
   resolveHookHomeDir,
   resolveServerEntry,
+  serverReplacementReason,
   shutdownServerOnPort,
   shouldForceRestartForLocalBuild,
   shouldKillProcessOnPort,
@@ -2638,6 +2639,25 @@ test("shouldRestartServer does not restart when /health was unreachable", () => 
   assert.equal(shouldRestartServer("0.1.4", null), false);
 });
 
+// Every other open review page is told why its server went away, so the reason has to name the
+// branch that actually fired: a local-build force replaces a server of the same version, and
+// calling that an update is false on both counts.
+test("serverReplacementReason names a local-build force apart from a real version change", () => {
+  assert.equal(serverReplacementReason("0.1.4", { ok: true, app: "atelier-axi", version: "0.1.3" }), "upgrade");
+  assert.equal(serverReplacementReason("0.1.4", { ok: true, app: "atelier-axi" }), "upgrade");
+  assert.equal(
+    serverReplacementReason("0.1.4", { ok: true, app: "atelier-axi", version: "0.1.4" }, true),
+    "local-build",
+  );
+  // A version difference is an upgrade even when the local-build force is also set.
+  assert.equal(serverReplacementReason("0.1.4", { ok: true, app: "atelier-axi", version: "0.1.3" }, true), "upgrade");
+});
+
+test("serverReplacementReason names nothing when no replacement is warranted", () => {
+  assert.equal(serverReplacementReason("0.1.4", { ok: true, app: "atelier-axi", version: "0.1.4" }), "");
+  assert.equal(serverReplacementReason("0.1.4", null), "");
+});
+
 test("shouldKillProcessOnPort does not kill unidentified health responders", () => {
   assert.equal(shouldKillProcessOnPort("0.1.4", { ok: true, app: "other", version: "0.1.3" }), false);
 });
@@ -2866,3 +2886,109 @@ async function startFakeHtmlApp(requests) {
     close: () => new Promise((resolve) => server.close(() => resolve())),
   };
 }
+
+// A stand-in for a running server of another version: it answers /health, records what the CLI
+// actually puts on the wire at /shutdown, and then frees the port like a real one.
+async function startShutdownRecorder(version = "0.0.0-previous") {
+  const bodies = [];
+  const server = createServer((req, res) => {
+    if (req.url === "/health") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(JSON.stringify({ ok: true, app: "atelier-axi", version }));
+      return;
+    }
+    if (req.url === "/shutdown" && req.method === "POST") {
+      let raw = "";
+      req.on("data", (chunk) => {
+        raw += chunk;
+      });
+      req.on("end", () => {
+        bodies.push(raw ? JSON.parse(raw) : {});
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ status: "shutting-down" }));
+        server.close();
+        server.closeAllConnections?.();
+      });
+      return;
+    }
+    res.writeHead(404).end();
+  });
+  await new Promise((resolve) => server.listen(0, "127.0.0.1", () => resolve(undefined)));
+  const address = /** @type {import("node:net").AddressInfo} */ (server.address());
+  return { bodies, port: address.port, close: () => server.close() };
+}
+
+test("atelier-axi stop tells the server it was stopped, and names no session to reload", async () => {
+  const recorder = await startShutdownRecorder();
+  try {
+    const output = await shutdownServerOnPort(recorder.port, {
+      baseUrl: `http://127.0.0.1:${recorder.port}`,
+      currentVersion: "0.1.4",
+    });
+
+    assert.deepEqual(recorder.bodies, [{ reason: "stop" }]);
+    assert.equal(output.server.status, "stopped");
+  } finally {
+    recorder.close();
+  }
+});
+
+// The page being opened is the one that reloads itself, and the server picks it by key, so the
+// key the CLI sends has to be the canonical session key for the file - an empty or non-canonical
+// one silently degrades the feature to "nobody reloads, everybody gets a banner".
+test("opening an artifact names that session as the one to reload across a version upgrade", async () => {
+  const recorder = await startShutdownRecorder();
+  const dir = await mkdtemp(path.join(os.tmpdir(), "atelier-open-reload-"));
+  const stateDir = path.join(dir, "state");
+  const nested = path.join(dir, "pages");
+  await mkdir(nested, { recursive: true });
+  const artifact = path.join(nested, "board.html");
+  await writeFile(artifact, "<!doctype html><html><body>board</body></html>");
+  const base = `http://127.0.0.1:${recorder.port}`;
+
+  try {
+    // Spawned asynchronously on purpose: the recorder runs in this process, so a blocking
+    // spawnSync would deadlock the CLI's own /health request against it.
+    const child = spawn(
+      process.execPath,
+      // A path with a `..` hop: the key must come from the canonicalized file, not this spelling.
+      [
+        fileURLToPath(new URL("../bin/atelier-axi.js", import.meta.url)),
+        path.join(nested, "..", "pages", "board.html"),
+        "--no-open",
+      ],
+      {
+        cwd: fileURLToPath(new URL("..", import.meta.url)),
+        env: {
+          ...process.env,
+          ATELIER_AXI_PORT: String(recorder.port),
+          ATELIER_AXI_STATE_DIR: stateDir,
+          ATELIER_AXI_TELEMETRY: "0",
+        },
+        stdio: ["ignore", "pipe", "pipe"],
+      },
+    );
+    let stderr = "";
+    child.stderr.on("data", (chunk) => {
+      stderr += chunk;
+    });
+    child.stdout.resume();
+    const code = await new Promise((resolve) => child.on("exit", resolve));
+
+    assert.equal(code, 0, stderr);
+    assert.deepEqual(recorder.bodies, [{ reload_key: sessionKey(await canonicalFile(artifact)), reason: "upgrade" }]);
+  } finally {
+    // The CLI replaced the recorder with a real server on that port; stop it again.
+    await fetch(`${base}/shutdown`, { method: "POST" }).catch(() => {});
+    for (let i = 0; i < 30; i += 1) {
+      const alive = await fetch(`${base}/health`).then(
+        () => true,
+        () => false,
+      );
+      if (!alive) break;
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+    recorder.close();
+    await rm(dir, { recursive: true, force: true });
+  }
+});